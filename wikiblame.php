8416bb8b15b0b43d162c083b5ff7864b462065f1
flominator <flominator@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2009-05-31 09:53:49 +0000

    found a better solution for turning off magic_quotes_gpc

--- wikiblame.php
+++ wikiblame.php
@@ -7,9 +7,28 @@
 		<title>WikiBlame</title>
 	</head><?
 
+// required to prevent automatic escaping of input strings
+// thanks to http://www.php.net/manual/de/security.magicquotes.disabling.php	
+if (get_magic_quotes_gpc()) {
+    function stripslashes_deep($value)
+    {
+        $value = is_array($value) ?
+                    array_map('stripslashes_deep', $value) :
+                    stripslashes($value);
+
+        return $value;
+    }
+
+    $_POST = array_map('stripslashes_deep', $_POST);
+    $_GET = array_map('stripslashes_deep', $_GET);
+    $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+    $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
+}
+	
 include("shared_inc/language.inc.php");
 include("shared_inc/wiki_functions.inc.php");
 $inc_dir = "wikiblame_inc";
+
 
 //get the language file and decide whether rtl oder ltr is used
 $user_lang = read_language();
