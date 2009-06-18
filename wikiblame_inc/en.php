7587b6f2514828e5b1b08c2613172fe53e732286
siebrand <siebrand@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2009-06-18 22:12:16 +0000

    Language and consistency updates

--- wikiblame_inc/en.php
+++ wikiblame_inc/en.php
@@ -4,12 +4,12 @@ $text_dir = 'ltr';
 //Form elements
 $messages['lang']='Language';
 $messages['project']='Project';
-$messages['article']='Article';
+$messages['article']='Page';
 $messages['needle']='Search for';
 $messages['skipversions']='Always skip x versions';
 $messages['ignorefirst']='Ignore first x versions';
 $messages['limit']='Versions to check';
-$messages['start_date'] = 'Starting date';
+$messages['start_date'] = 'Start date';
 $messages['order'] = 'Order';
 $messages['newest_first'] = 'latest first';
 $messages['oldest_first'] = 'oldest first';
@@ -28,7 +28,7 @@ $messages['contact_link']='http://de.wikipedia.org/wiki/Benutzer Diskussion:Flom
 $messages['wrong_skips'] = 'Wrong settings: If the first __VERSIONSTOSKIP__ versions are skipped, then none of the __VERSIONSTOSEARCH__ versions to be searched will be processed.';
 
 $messages['search_in_progress'] = 'The version history of _ARTICLELINK_ is being searched for <b>_NEEDLE_</b> ...';
-$messages['execution_time'] = 'Execution Time: _EXECUTIONTIME_ seconds';
+$messages['execution_time'] = 'Execution time: _EXECUTIONTIME_ seconds';
 $messages['versions_found'] = '_NUMBEROFVERSIONS_ versions found';
 
 $messages['binary_test'] = 'Comparing differences in _FIRSTDATEVERSION_ between _FIRSTNUMBER_ and _SECONDNUMBER_ while coming from _SOURCENUMBER_: ';
