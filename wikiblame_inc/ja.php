e1ab9a6919c77043ed5ed47e27d4ddae604c4d78
siebrand <siebrand@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2009-06-19 19:34:11 +0000

    Localisation updates from http://translatewiki.net (2009-06-19 19:23 UTC)

---
+++ wikiblame_inc/ja.php
@@ -0,0 +1,42 @@
+<?php
+/** Wikiblame
+ *
+ */
+
+/** Japanese (日本語)
+ *
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Fryed-peach
+ */
+
+$messages['lang'] = '言語';
+$messages['project'] = 'プロジェクト';
+$messages['article'] = 'ページ';
+$messages['needle'] = '検索語';
+$messages['skipversions'] = '間の何版かの検査をとばす';
+$messages['ignorefirst'] = '初めの何版かの検査をとばす';
+$messages['limit'] = '検査する版数';
+$messages['start_date'] = '開始日';
+$messages['order'] = '順序';
+$messages['newest_first'] = '新しいものを初めに';
+$messages['oldest_first'] = '古いものを初めに';
+$messages['search_method'] = '探索方法';
+$messages['linear'] = '線型';
+$messages['interpolated'] = '補間 (版が多い場合に早い)';
+$messages['ignore_minors'] = '細部の編集は無視する (実験的)';
+$messages['start'] = '開始';
+$messages['reset'] = 'リセット';
+$messages['manual'] = '使用説明';
+$messages['contact'] = '連絡先';
+$messages['wrong_skips'] = '設定の不備: 初めの__VERSIONSTOSKIP__版をとばしてしまうと、検査すべき__VERSIONSTOSEARCH__版のどれも処理されないことになります。';
+$messages['search_in_progress'] = '_ARTICLELINK_の履歴中で<b>_NEEDLE_</b>を検索しています…';
+$messages['execution_time'] = '実行時間: _EXECUTIONTIME_秒';
+$messages['versions_found'] = '_NUMBEROFVERSIONS_版が見つかりました';
+$messages['binary_test'] = '_SOURCENUMBER_番の次に、_FIRSTDATEVERSION_における_FIRSTNUMBER_番と_SECONDNUMBER_番の違いを比較しています:';
+$messages['insertion_found'] = 'LEFT_VERSION と RIGHT_VERSION の間で挿入されました';
+$messages['deletion_found'] = 'LEFT_VERSION と RIGHT_VERSION の間で削除されました';
---
+++ wikiblame_inc/no.php
@@ -0,0 +1,42 @@
+<?php
+/** Wikiblame
+ *
+ */
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ *
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Nghtwlkr
+ */
+
+$messages['lang'] = 'Språk';
+$messages['project'] = 'Prosjekt';
+$messages['article'] = 'Side';
+$messages['needle'] = 'Søk etter';
+$messages['skipversions'] = 'Alltid hopp over x versjoner';
+$messages['ignorefirst'] = 'Ignorer de første x versjonene';
+$messages['limit'] = 'Versjoner som skal sjekkes';
+$messages['start_date'] = 'Startdato';
+$messages['order'] = 'Sorter';
+$messages['newest_first'] = 'siste først';
+$messages['oldest_first'] = 'eldste først';
+$messages['search_method'] = 'Søkemetode';
+$messages['linear'] = 'lineær';
+$messages['interpolated'] = 'interpolert (raskere med flere versjoner)';
+$messages['ignore_minors'] = 'ignorer mindre endringer (eksperimentell)';
+$messages['start'] = 'Start';
+$messages['reset'] = 'Nullstill';
+$messages['manual'] = 'Manual';
+$messages['contact'] = 'Kontakt';
+$messages['wrong_skips'] = 'Feil innstillinger: Dersom de første __VERSIONTOSKIP__ versjonene hoppes over, så vil ingen av de __VERSIONSTOSEARCH__ versjonene som skal søkes etter bli gjennomført.';
+$messages['search_in_progress'] = 'Versjonshistorikken til _ARTICLELINK_ søkes etter <b>_NEEDLE_</b> ...';
+$messages['execution_time'] = 'Kjøretid: _EXECUTIONTIME_ sekunder';
+$messages['versions_found'] = '_NUMBEROFVERSIONS_ versjoner funnet';
+$messages['binary_test'] = 'Sammenlinger forskjellene i _FIRSTDATEVERSION_ mellom _FIRSTNUMBER_ og _SECONDNUMBER_ som kommer fra _SOURCENUMBER_:';
+$messages['insertion_found'] = 'Innsettelse funnet mellom LEFT_VERSION og RIGHT_VERSION';
+$messages['deletion_found'] = 'Sletting funnet mellom LEFT_VERSION og RIGHT_VERSION';
