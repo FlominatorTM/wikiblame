4047adfcce70e389f8dd1698b6b3dac2ea7401c7
Siebrand Mazeland <Siebrand@users.noreply.github.com>
2010-01-01 18:31:11 +0000

    Localisation updates from http://translatewiki.net (2010-01-01)

---
+++ wikiblame_inc/et.php
@@ -0,0 +1,34 @@
+<?php
+/** Wikiblame
+ *
+ */
+
+/** Estonian (Eesti)
+ *
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Avjoska
+ * @author Pikne
+ */
+
+$messages['January'] = 'jaanuar';
+$messages['February'] = 'veebruar';
+$messages['March'] = 'märts';
+$messages['April'] = 'aprill';
+$messages['May'] = 'mai';
+$messages['June'] = 'juuni';
+$messages['July'] = 'juuli';
+$messages['August'] = 'august';
+$messages['September'] = 'september';
+$messages['October'] = 'oktoober';
+$messages['November'] = 'november';
+$messages['December'] = 'detsember';
+$messages['lang'] = 'Keel';
+$messages['project'] = 'Projekt';
+$messages['article'] = 'Lehekülg';
+$messages['newest_first'] = 'uuemad enne';
+$messages['oldest_first'] = 'vanemad enne';
--- wikiblame_inc/nn.php
+++ wikiblame_inc/nn.php
@@ -12,7 +12,45 @@
  * @file
  *
  * @author Gunnernett
+ * @author Harald Khan
  */
 
+$messages['January'] = 'Januar';
+$messages['February'] = 'Februar';
+$messages['March'] = 'Mars';
+$messages['April'] = 'April';
+$messages['May'] = 'Mai';
+$messages['June'] = 'Juni';
+$messages['July'] = 'Juli';
+$messages['August'] = 'August';
+$messages['September'] = 'September';
+$messages['October'] = 'Oktober';
+$messages['November'] = 'November';
+$messages['December'] = 'Desember';
 $messages['lang'] = 'Språk';
+$messages['project'] = 'Prosjekt';
+$messages['article'] = 'Sida';
+$messages['needle'] = 'Søk etter';
+$messages['skipversions'] = 'Alltid hopp over x versjonar';
+$messages['ignorefirst'] = 'Ignorer dei x fyrste versjonane';
+$messages['limit'] = 'Versjonar som skal verta sjekka';
+$messages['start_date'] = 'Startdato';
+$messages['order'] = 'Rekkjefylgja';
+$messages['newest_first'] = 'siste fyrst';
+$messages['oldest_first'] = 'eldste fyrst';
+$messages['search_method'] = 'Søkjemetode';
+$messages['linear'] = 'lineær';
+$messages['interpolated'] = 'interpolert (raskare med fleire versjonar)';
+$messages['ignore_minors'] = 'ignorer mindre endringar (eksperimental)';
+$messages['start'] = 'Start';
+$messages['reset'] = 'Nullstill';
+$messages['manual'] = 'Manual';
+$messages['contact'] = 'Kontakt';
+$messages['wrong_skips'] = 'Gale innstillingar: Om dei __VERSIONSTOSKIP__ fyrste versjonane vert hoppa over, so vil ingen av dei __VERSIONSTOSEARCH__ versjonane som skulle verta gjennomsøkte verta handsama.';
+$messages['search_in_progress'] = 'Det vert søkt etter <b>_NEEDLE_</b> i versjonshistorikken åt _ARTICLELINK_...';
+$messages['execution_time'] = 'Køyretid: _EXECUTIONTIME_ sekund';
+$messages['versions_found'] = 'Fann _NUMBEROFVERSIONS_ versjonar';
+$messages['binary_test'] = 'Samanliknar skilnader i _FIRSTDATEVERSION_ mellom _FIRSTNUMBER_ og _SECONDNUMBER_ som kjem frå _SOURCENUMBER_:';
+$messages['insertion_found'] = 'Innsetjing funnen mellom LEFT_VERSION og RIGHT_VERSION';
+$messages['deletion_found'] = 'Sletting funnen mellom LEFT_VERSION og RIGHT_VERSION';
 $messages['help_translating'] = 'Hjelp til med omsetjing på translatewiki.net';
