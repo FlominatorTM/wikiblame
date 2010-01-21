b6914c518b1a6f8bed77b3f737097bdbd278d26d
Siebrand Mazeland <Siebrand@users.noreply.github.com>
2010-01-21 22:36:32 +0000

    Localisation updates from http://translatewiki.net (2010-01-21)

---
+++ wikiblame_inc/uk.php
@@ -0,0 +1,17 @@
+<?php
+/** Wikiblame
+ *
+ */
+
+/** Ukrainian (Українська)
+ *
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Prima klasy4na
+ */
+
+$messages['lang'] = 'Мова';
