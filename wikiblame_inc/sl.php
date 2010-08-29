8c241b0a2aaac83375b7048c50eb7c7bb8859599
siebrand <siebrand@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2010-08-29 18:00:38 +0000

    Localisation updates from http://translatewiki.net

--- wikiblame_inc/sl.php
+++ wikiblame_inc/sl.php
@@ -10,7 +10,46 @@
  * @ingroup Language
  * @file
  *
+ * @author Dbc334
  * @author Flominator
  */
 
+$messages['January'] = 'januar';
+$messages['February'] = 'februar';
+$messages['March'] = 'marec';
+$messages['April'] = 'april';
+$messages['May'] = 'maj';
+$messages['June'] = 'junij';
+$messages['July'] = 'julij';
+$messages['August'] = 'avgust';
+$messages['September'] = 'september';
+$messages['October'] = 'oktober';
+$messages['November'] = 'november';
+$messages['December'] = 'december';
+$messages['ui_lang'] = 'Prikazni jezik';
+$messages['lang'] = 'Jezik';
+$messages['project'] = 'Projekt';
+$messages['article'] = 'Stran';
+$messages['needle'] = 'Išči za:';
+$messages['skipversions'] = 'Vedno preskoči x različic';
+$messages['ignorefirst'] = 'Prezri prvih x različic';
+$messages['limit'] = 'Različice za preverjanje';
+$messages['start_date'] = 'Datum začetka';
+$messages['order'] = 'Vrstni red';
+$messages['newest_first'] = 'novejši najprej';
+$messages['oldest_first'] = 'starejši najprej';
+$messages['search_method'] = 'Način iskanja';
+$messages['binary'] = 'binarno';
 $messages['binary_in_wp'] = 'http://sl.wikipedia.org/wiki/Binarno_iskanje';
+$messages['linear'] = 'linearno';
+$messages['interpolated'] = 'interpolirano (hitrejše pri več različicah)';
+$messages['ignore_minors'] = 'prezri manjše spremembe (preizkusno)';
+$messages['force_wikitags'] = 'prisili iskanje wikibesedila';
+$messages['start'] = 'Začni';
+$messages['reset'] = 'Ponastavi';
+$messages['manual'] = 'Priročnik';
+$messages['contact'] = 'Stik';
+$messages['execution_time'] = 'Čas izvajanja: _EXECUTIONTIME_ sekund';
+$messages['versions_found'] = 'najdenih _NUMBEROFVERSIONS_ različic';
+$messages['help_translating'] = 'Pomagajte prevajati na translatewiki.net';
+$messages['start_here'] = 'Išči od tukaj';
