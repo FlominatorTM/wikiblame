3d4f7869336a3e35ec96e8c49cb0949fa2d893aa
Siebrand Mazeland <Siebrand@users.noreply.github.com>
2012-05-12 18:43:49 +0000

    Localisation updates from http://translatewiki.net.

--- wikiblame_inc/nan.php
+++ wikiblame_inc/nan.php
@@ -13,21 +13,3 @@
  * @author Ianbu
  */
 
-$messages['January'] = '1-goe̍h';
-$messages['February'] = '2-goe̍h';
-$messages['March'] = '3-goe̍h';
-$messages['April'] = '4-goe̍h';
-$messages['May'] = '5-goe̍h';
-$messages['June'] = '6-goe̍h';
-$messages['July'] = '7-goe̍h';
-$messages['August'] = '8-goe̍h';
-$messages['September'] = '9-goe̍h';
-$messages['October'] = '10-goe̍h';
-$messages['November'] = '11-goe̍h';
-$messages['December'] = '12-goe̍h';
-$messages['ui_lang'] = 'Hián-sī oē-gí';
-$messages['lang'] = 'Oē-gí';
-$messages['project'] = 'Sū-kang';
-$messages['article'] = 'Bûn-chiuⁿ';
-$messages['needle'] = 'Chhoé';
-$messages['start_date'] = 'Khai-sí ê ji̍t-jí';
