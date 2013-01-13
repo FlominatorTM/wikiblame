9173efce62f7d034c72e2596433050ca358583db
siebrand <siebrand@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2013-01-13 15:01:44 +0000

    Localisation updates from http://translatewiki.net.

--- wikiblame_inc/az.php
+++ wikiblame_inc/az.php
@@ -10,6 +10,7 @@
  * @ingroup Language
  * @file
  *
+ * @author AZISS
  * @author Cekli829
  * @author Sortilegus
  */
@@ -26,11 +27,22 @@ $messages['September'] = 'Sentyabr';
 $messages['October'] = 'Oktyabr';
 $messages['November'] = 'Noyabr';
 $messages['December'] = 'Dekabr';
+$messages['ui_lang'] = 'Displey dili';
 $messages['lang'] = 'Dil';
 $messages['project'] = 'Layihə';
 $messages['article'] = 'Səhifə';
+$messages['ignorefirst'] = 'İlk x versiyaya məhəl qoymamaq';
+$messages['start_date'] = 'Başlanğıc tarixi';
+$messages['date_format'] = 'DD MM, YYYY';
 $messages['order'] = 'Ardıcıllıq';
+$messages['search_method'] = 'Axtarış metodu';
+$messages['ignore_minors'] = 'Xırda dəyişiklikləri məhəl qoymamaq (eksperimental)';
+$messages['force_wikitags'] = 'Wikitext üçün məcburi axtarış';
 $messages['start'] = 'Başla';
 $messages['reset'] = 'Qur';
 $messages['manual'] = 'Əl ilə';
 $messages['contact'] = 'Əlaqə';
+$messages['earlier_versions_available'] = 'Ehtimal ki, daha erkən versiyalar var.';
+$messages['execution_time'] = 'İcra vaxtı: _EXECUTIONTIME_ saniyə';
+$messages['versions_found'] = '_NUMBEROFVERSIONS_ versiya tapılıb';
+$messages['please_wait'] = 'Lütfən gözləyin...';
