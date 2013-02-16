886730612fe011b2898471bf47906913ec010574
flominator <flominator@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2013-02-16 15:20:47 +0000

    massive refatoring

--- wbw_rating.php
+++ wbw_rating.php
@@ -1,57 +1,97 @@
-<?header('Content-Type: text/html; charset=utf-8');  ?>
-<h1>Zwischenstände</h1>
+<?header('Content-Type: text/html; charset=utf-8');  ?><h1>Zwischenstände</h1>
 <?
-//shows the entries from zukunft that have been removed
-include("shared_inc/wiki_functions.inc.php");
-
-//$article = "Benutzer:Flominator/Zukunft";
-
-$edition = $_REQUEST['edition'];
-$article = "Wikipedia:Wartungsbausteinwettbewerb/".name_in_url($edition);
-$lang = "de";
-$project = "wikipedia";
-$server = "$lang.$project.org";
+$time_on_wm_server = time() - 7*60*60 ; //change according to http://www.zeitzonen.de/central_standard_time_cst_-_usa.html
 $is_debug = ($_REQUEST['debug']=="on" || $_REQUEST['debug']=="true" );
 
-$wbw_page = "http://".$server."/w/index.php?title=$article";
-$html_page = get_request($server, $wbw_page, true );
+include("shared_inc/wiki_functions.inc.php");
+$server = "$lang.$project.org";
 
-$revision_prefix = "\"wgCurRevisionId\":";
-$index_of_revision_id = strpos($html_page, $revision_prefix );
-$end_of_revision_id = strpos($html_page, ",", $index_of_revision_id);
+$wbw_page = "http://".$server."/w/index.php?title="."Wikipedia:Wartungsbausteinwettbewerb/".name_in_url($_REQUEST['edition']);
 
-$oldid = substr($html_page, $index_of_revision_id + strlen($revision_prefix ), $end_of_revision_id -$index_of_revision_id - strlen($revision_prefix ) );
+$points_per_team = rate_teams($server, $wbw_page);
 
+sort_and_print_score_list($points_per_team);
+print_form($wbw_page, update_paragraphs(get_source_code_paragraphs($server, $wbw_page), $points_per_team));
 
-$team_paragraphs = explode("h6>", $html_page);
-
-$points_per_team;//[] = array("Team"=> "Dummy", "Points"=>"-1");
-
-for($iTeam = 1;$iTeam<count($team_paragraphs);$iTeam++)
+function extract_user_name_column($list_of_article_points)
 {
-	//team name
-	$team_name = str_replace("[Bearbeiten]", "", strip_tags($team_paragraphs[$iTeam]));
+	global $is_debug;
+	$i = 0;
+	$firstColumn = $list_of_article_points[$i];
+	$list_of_article_points[$i]=""; //remove first column
 	
-	$iTeam++;
-	$points_of_this_team = 0;
-	$list_of_article_points = explode("(", $team_paragraphs[$iTeam]);
-	
-	//user names
-	$iEndOfFirstColumn = strpos($list_of_article_points[0], "</td>");
-	$allUserNames = substr($list_of_article_points[0], 0, $iEndOfFirstColumn);
+	while(!stristr($firstColumn, "</td>"))
+	{
+		if($is_debug) echo "table didn't end in first column, adding next<br>";
+		$i++;
+		if($i==count($list_of_article_points)) 
+		{
+			die("no table end found");
+		}
+		$firstColumn = $firstColumn . $list_of_article_points[$i];
+		$list_of_article_points[$i] = "";
+	}
+	$iEndOfFirstColumn = strpos($firstColumn, "</td>");
+	if($is_debug) echo "end of first column:" . $iEndOfFirstColumn. "<br>";
+	return  substr($firstColumn, 0, $iEndOfFirstColumn);
+}
+
+function rate_teams($server, $wbw_page)
+{
+	global $is_debug, $html_page;
+	$html_page = get_request($server, $wbw_page, true );
+	$team_paragraphs = explode("h6>", $html_page);
+	$points_per_team;//[] = array("Team"=> "Dummy", "Points"=>"-1");
+
+	for($iTeam = 1;$iTeam<count($team_paragraphs);$iTeam++)
+	{
+		//team name
+		$team_name = str_replace("[Bearbeiten]", "", strip_tags($team_paragraphs[$iTeam]));
+		if($is_debug) echo "Team=$team_name <br>";
+		$iTeam++; // ignore next part (end of h6-Tag)
+		
+		$list_of_article_points = explode("(", $team_paragraphs[$iTeam]);
+			
+		$userNameColumn = extract_user_name_column(&$list_of_article_points); //by ref!!!
+		$numberOfTeamMembers = get_number_of_users($userNameColumn);
+		
+		$points_of_this_team = count_points_of_team($list_of_article_points);
+
+		$totalPointsOfThisTeam = $points_of_this_team;
+		if($numberOfTeamMembers>3)
+		{
+			$points_of_this_team = 3* ($points_of_this_team / $numberOfTeamMembers);
+		}
+		$points_per_team[] = array("Team" => $team_name, "Points" => $points_of_this_team, "NumberMembers" => $numberOfTeamMembers, "TotalPoints" => $totalPointsOfThisTeam);
+		//echo "Team:" . $team_name . "Points:" . $points_of_this_team;
+		//echo $points_per_team[count($points_per_team)-1]["Team"]."//".$points_per_team[count($points_per_team)-1]["Points"];
+		if($is_debug) echo "<hr>";
+	}
+	return $points_per_team;
+}
+
+function get_number_of_users($allUserNames)
+{
+	global $is_debug;
+	if($is_debug) echo "allUserNames=".htmlspecialchars($allUserNames);
 	$userLines = explode("<li><a href", $allUserNames);
 	$numberOfTeamMembers = 0;
 	foreach($userLines as $oneUserLine)
 	{
-		//echo "line is $oneUserLine";
-		if(stristr($oneUserLine, "Benutzer"))
+		if ($is_debug) echo "line is $oneUserLine <br>";
+		$oneLineLower = strtolower($oneUserLine);
+		if(stristr($oneUserLine, "benutzer") ||stristr($oneUserLine, "user"))
 		{
 			$numberOfTeamMembers++;
 		}
 	}
-	//echo $allUserNames . " has members: $numberOfTeamMembers";
-	
-	//points
+	if($is_debug) echo "Team has " + $numberOfTeamMembers + " members";
+	return $numberOfTeamMembers;
+}
+
+function count_points_of_team ($list_of_article_points)
+{
+	$points_of_this_team = 0;
 	foreach($list_of_article_points as $one_rated_article)
 	{
 		//echo "<hr>";
@@ -70,64 +110,68 @@ for($iTeam = 1;$iTeam<count($team_paragraphs);$iTeam++)
 			}
 		}
 	}
-	$totalPointsOfThisTeam = $points_of_this_team;
-	if($numberOfTeamMembers>3)
+	return $points_of_this_team;
+}
+	
+function sort_and_print_score_list($points_per_team)
+{
+	foreach ($points_per_team as $nr => $inhalt)
 	{
-		$points_of_this_team = 3* ($points_of_this_team / $numberOfTeamMembers);
+		$points[$nr]  = strtolower( $inhalt['Points'] );
 	}
-	$points_per_team[] = array("Team" => $team_name, "Points" => $points_of_this_team, "NumberMembers" => $numberOfTeamMembers, "TotalPoints" => $totalPointsOfThisTeam);
-	//echo "Team:" . $team_name . "Points:" . $points_of_this_team;
-	//echo $points_per_team[count($points_per_team)-1]["Team"]."//".$points_per_team[count($points_per_team)-1]["Points"];
-}
 
-foreach ($points_per_team as $nr => $inhalt)
-{
-	$points[$nr]  = strtolower( $inhalt['Points'] );
-}
+	array_multisort($points, SORT_DESC, $points_per_team);
 
-array_multisort($points, SORT_DESC, $points_per_team);
+	//print_r (  $points_per_team );
 
-//print_r (  $points_per_team );
-
-echo "<ol>";
-for($i=0;$i<count($points_per_team);$i++)
-{
-	echo "<li>" . $points_per_team[$i]["Team"].":".$points_per_team[$i]["Points"]."</li>";
-}
-echo "</ol>";
-
-
-$wbw_page_raw = $wbw_page. "&action=raw";
-$source_code_page = removeheaders(get_request($server, $wbw_page_raw, true )); 
-
-$paragraphs = explode("\n======", $source_code_page);
-
-for($iParagraph=0;$iParagraph<count($paragraphs );$iParagraph++)
-{
-	if($is_debug) echo "<h1>Para $iParagraph before</h1>".htmlspecialchars($paragraphs[$iParagraph])."<hr>";
-
+	echo "<ol>";
 	for($i=0;$i<count($points_per_team);$i++)
 	{
-		$i_team_length = strlen($points_per_team[$i]["Team"]);
-		if(substr($paragraphs[$iParagraph], 0, $i_team_length)==$points_per_team[$i]["Team"])
-		{
-			$paragraphs[$iParagraph] = update_one_team_paragraph($paragraphs[$iParagraph], $points_per_team[$i]);
-			$i = count($points_per_team);
-		}
+		echo "<li>" . $points_per_team[$i]["Team"].":".$points_per_team[$i]["Points"]."</li>";
 	}
-	if($is_debug) echo "<h1>Para $iParagraph after</h1>".htmlspecialchars($paragraphs[$iParagraph])." <hr>";
+	echo "</ol>";
 }
 
-print_form($wbw_page, $paragraphs);
-//echo "<a href=\"". $wbw_page . "&action=edit&summary=Zwischenstände aktualisiert\">bearbeiten</a><br>";
+function get_source_code_paragraphs($server, $wbw_page)
+{
+	$wbw_page_raw = $wbw_page. "&action=raw";
+	$source_code_page = removeheaders(get_request($server, $wbw_page_raw, true )); 
 
+	$paragraphs = explode("\n======", $source_code_page);
+	return $paragraphs;
+}
+
+function update_paragraphs($paragraphs, $points_per_team)
+{
+	global $is_debug;
+	for($iParagraph=0;$iParagraph<count($paragraphs );$iParagraph++)
+	{
+		if($is_debug) echo "<h1>Para $iParagraph before</h1>".htmlspecialchars($paragraphs[$iParagraph])."<hr>";
+
+		for($i=0;$i<count($points_per_team);$i++)
+		{
+			$i_team_length = strlen($points_per_team[$i]["Team"]);
+			if($is_debug) echo "i_team_length=$i_team_length";
+			if($is_debug) echo "comparing \"" . htmlspecialchars(substr($paragraphs[$iParagraph], 0, $i_team_length)) . "\" to  \"<b>" . $points_per_team[$i]["Team"]."\"</b><br>";
+			if(substr(htmlspecialchars($paragraphs[$iParagraph]), 0, $i_team_length)==$points_per_team[$i]["Team"])
+			{
+				if($is_debug) echo "found team " . $points_per_team[$i]["Team"];
+				$paragraphs[$iParagraph] = update_one_team_paragraph($paragraphs[$iParagraph], $points_per_team[$i]);
+				$i = count($points_per_team);
+			}
+		}
+		if($is_debug) echo "<h1>Para $iParagraph after</h1>".htmlspecialchars($paragraphs[$iParagraph])." <hr>";
+	}
+	return $paragraphs;
+}
+	
 function print_form($wbw_page, $paragraphs)
 {
+	global $time_on_wm_server;
 	echo '<form method="post" action="'.$wbw_page.'&action=submit">';
 	echo '<input type="submit" value="Update"/><br>';
 	echo '<input type="hidden" name="wpSummary" value="Zwischenstände aktualisiert"/>';
 	echo '<input type="hidden" name="wpDiff" value="Zwischenstände aktualisiert"/>';
-	$time_on_wm_server = time() - 7*60*60 ; //change according to http://www.zeitzonen.de/central_standard_time_cst_-_usa.html
 	echo '<input type="hidden" name="wpStarttime" value="'. strftime("%Y%m%d%H%M%S", $time_on_wm_server). '" />';
 	echo '<input type="hidden" name="wpEdittime" value="" />';
 	
@@ -179,9 +223,10 @@ function get_place_for_template_insertion($one_paragraph)
 
 function get_result_template($point_set_this_team)
 {
-	global $article, $oldid;
+	global $wbw_page;
+	$oldid = get_old_id();
 	$anchor = sprintf("%04d",ceil($point_set_this_team["Points"]));
-	$permalink = "[http://de.wikipedia.org/w/index.php?title=".str_replace(" ", "_", $article)."&oldid=$oldid ". strftime("%d.%m.")."]";
+	$permalink = "[".$wbw_page."&oldid=".$oldid . strftime(" %d.%m.")."]";
 
 	$points_of_this_team =  $point_set_this_team["Points"];
 	$result_param = "Ergebnis=$points_of_this_team";
@@ -194,9 +239,20 @@ function get_result_template($point_set_this_team)
 	return $text_to_insert;
 }
 
+function get_old_id()
+{
+	global $is_debug, $html_page;
+	$revision_prefix = "\"wgCurRevisionId\":";
+	$index_of_revision_id = strpos($html_page, $revision_prefix );
+	$end_of_revision_id = strpos($html_page, ",", $index_of_revision_id);
+
+	$oldid = substr($html_page, $index_of_revision_id + strlen($revision_prefix ), $end_of_revision_id -$index_of_revision_id - strlen($revision_prefix ) );
+	if($is_debug) echo "oldid=". $oldid;
+	return $oldid;
+}
 function update_one_team_paragraph($one_paragraph, $point_set_this_team)
 {
-	global $article, $oldid, $is_debug;
+	global $is_debug;
 	$one_paragraph = remove_existing_result_template($one_paragraph);
 	$index_for_insertion = get_place_for_template_insertion($one_paragraph);
 	$result_template = get_result_template($point_set_this_team);
