047ae3ec265683e59dc55af0e5169c858527b8e0
siebrand <siebrand@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2013-04-14 11:57:33 +0000

    Localisation updates from http://translatewiki.net.

--- wikiblame_inc/or.php
+++ wikiblame_inc/or.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** Oriya (ଓଡ଼ିଆ)
+/** Oriya (ଓଡ଼ିଆ)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
