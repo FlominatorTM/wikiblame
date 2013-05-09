fb53601de18e09f3180c50ffe56e2f06e363ddd9
siebrand <siebrand@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2013-05-09 13:42:22 +0000

    Localisation updates from http://translatewiki.net.

--- wikiblame_inc/la.php
+++ wikiblame_inc/la.php
@@ -10,7 +10,9 @@
  * @ingroup Language
  * @file
  *
+ * @author Autokrator
  * @author MissPetticoats
+ * @author UV
  */
 
 $messages['January'] = 'Ianuarius';
@@ -23,7 +25,31 @@ $messages['July'] = 'Iulius';
 $messages['August'] = 'Augustus';
 $messages['September'] = 'September';
 $messages['October'] = 'October';
+$messages['November'] = 'November';
+$messages['December'] = 'December';
+$messages['ui_lang'] = 'Linguam monstrare';
 $messages['lang'] = 'Lingua';
+$messages['project'] = 'Inceptum';
 $messages['article'] = 'Pagina';
-$messages['date_format'] = ' MM DD, YYYY/AAAA?';
+$messages['needle'] = 'Quaerere';
+$messages['skipversions'] = 'Semper x recensiones praeterire';
+$messages['ignorefirst'] = 'Neglegere primae x recensiones';
+$messages['limit'] = 'Recensiones examinandae';
+$messages['start_date'] = 'Exordium';
+$messages['date_format'] = ' MM DD, YYYY';
+$messages['order'] = 'Ordo';
+$messages['newest_first'] = 'recentissimae primum';
+$messages['oldest_first'] = 'veterrimae primum';
+$messages['search_method'] = 'Ratio quaerendi';
+$messages['binary'] = 'binaria';
+$messages['linear'] = 'linearis';
+$messages['interpolated'] = 'binaris (celerior in multis recensionibus perscrutandis)';
+$messages['ignore_minors'] = 'Recensiones minores neglegere';
 $messages['start'] = 'Inceptare';
+$messages['reset'] = 'Reficere';
+$messages['manual'] = 'Manuale';
+$messages['no_differences'] = 'Nulla discrimina inter recensiones scrutatas inventa.';
+$messages['earlier_versions_available'] = 'Recensiones priores verisimiliter exstant.';
+$messages['once_more'] = 'Iterum:';
+$messages['help_translating'] = 'Interpretationi Latinae manum admovere';
+$messages['start_here'] = 'Abhinc quaerere';
