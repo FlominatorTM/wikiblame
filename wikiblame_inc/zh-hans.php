6a286675cf8d2c7ddfe958e97acf3e61af7335e4
Siebrand Mazeland <Siebrand@users.noreply.github.com>
2013-07-30 17:46:44 +0000

    Localisation updates from http://translatewiki.net.

--- wikiblame_inc/zh-hans.php
+++ wikiblame_inc/zh-hans.php
@@ -10,6 +10,7 @@
  * @ingroup Language
  * @file
  *
+ * @author Bencmq
  * @author Dimension
  * @author Hydra
  * @author PhiLiP
@@ -35,7 +36,7 @@ $messages['lang'] = '语言';
 $messages['project'] = '项目';
 $messages['article'] = '页面';
 $messages['needle'] = '搜索';
-$messages['skipversions'] = '总是跳过x版本';
+$messages['skipversions'] = '总是跳过x个版本';
 $messages['ignorefirst'] = '忽略开始的x个版本';
 $messages['limit'] = '要检查的版本数量';
 $messages['start_date'] = '开始日期';
@@ -43,35 +44,35 @@ $messages['date_format'] = 'YYYY MM DD';
 $messages['order'] = '排序';
 $messages['newest_first'] = '从最近开始';
 $messages['oldest_first'] = '从最早开始';
-$messages['binary_search_inverse'] = '寻找已去除的文本（仅限折半搜索）';
+$messages['binary_search_inverse'] = '搜索移除相关文本的编辑（仅限二分法搜索）';
 $messages['search_method'] = '搜索方式';
-$messages['binary'] = '折半';
+$messages['binary'] = '二分法';
 $messages['linear'] = '线性';
-$messages['interpolated'] = '折半（在更多的版本中更快）';
+$messages['interpolated'] = '二分法（在版本更多时更快）';
 $messages['ignore_minors'] = '忽略小修改（试验性）';
 $messages['force_wikitags'] = '强制搜索 wikitext';
 $messages['start'] = '开始';
 $messages['reset'] = '重置';
 $messages['manual'] = '手册';
 $messages['contact'] = '联系';
-$messages['get_less_versions'] = '您的搜索可能一次查询 __NUMREVISIONS__ 个修订版本。为了保护服务器，您的每次调用只被允许查询 __ALLOWEDREVISIONS__ 个修订。请更改设置或切换查询模式到折半搜索！';
-$messages['wrong_skips'] = '错误的设置：如果跳过首__VERSIONSTOSKIP__版本，那么要搜索的__VERSIONSTOSEARCH__个版本将不会被处理。';
+$messages['get_less_versions'] = '您的搜索可能一次查询 __NUMREVISIONS__ 个修订版本。为了保护服务器，您的每次调用只被允许查询 __ALLOWEDREVISIONS__ 个修订。请更改设置或切换查询模式到二分法搜索！';
+$messages['wrong_skips'] = '错误的设置：如果跳过__VERSIONSTOSKIP__个版本，那么要搜索的__VERSIONSTOSEARCH__个版本将不会被处理。';
 $messages['search_in_progress_text'] = '_ARTICLELINK_的版本记录正在以<b>_NEEDLE_</b>作为纯文本进行搜索';
 $messages['search_in_progress_wikitags'] = '_ARTICLELINK_的版本记录正在以<b>_NEEDLE_</b>作为wiki文本进行搜索';
-$messages['no_differences'] = '搜索修订中发现的没有差异。';
-$messages['first_version'] = '必须在第一个或最新的版本中发生了更改吗？';
+$messages['no_differences'] = '搜索修订中没有发现差异。';
+$messages['first_version'] = '更改是在第一个或最新的版本中发生的吧？';
 $messages['first_version_present'] = '__NEEDLE__已被发现可以追潮到自首__REVISIONLINK__的首个版本中存在。';
 $messages['earlier_versions_available'] = '可能存在更早的修订版本。';
 $messages['execution_time'] = '执行时间： _EXECUTIONTIME_ 秒';
-$messages['versions_found'] = '发现的 _NUMBEROFVERSIONS_ 版本';
+$messages['versions_found'] = '发现 _NUMBEROFVERSIONS_ 个版本';
 $messages['please_wait'] = '请稍等 …';
-$messages['binary_test'] = '在来自 _SOURCENUMBER_ 时，正在比较在 _FIRSTNUMBER_ 和 _SECONDNUMBER_ 之间，_FIRSTDATEVERSION_ 的差异：';
-$messages['dead_end'] = '抓住了一些死胡同（可能造成的归还或编辑大战）';
-$messages['once_more'] = '再一次，有感而发：';
-$messages['binary_enough'] = '执行足够的重试次数，条历史很乱了，请试着更改某些设置。';
-$messages['insertion_found'] = 'LEFT_VERSION 和 RIGHT_VERSION 之间发现了插入';
-$messages['deletion_found'] = 'LEFT_VERSION 和 RIGHT_VERSION 之间发现了删除';
+$messages['binary_test'] = '由 _SOURCENUMBER_ 开始，正在比较在 _FIRSTNUMBER_ 和 _SECONDNUMBER_ 之间的差异 _FIRSTDATEVERSION_ ：';
+$messages['dead_end'] = '发现一些循环（可能由回退或编辑战引起）';
+$messages['once_more'] = '再来一次：';
+$messages['binary_enough'] = '重试次数够多了，条目历史太乱了，请试着更改某些设置。';
+$messages['insertion_found'] = '目标在 LEFT_VERSION 和 RIGHT_VERSION 之间被加入';
+$messages['deletion_found'] = '目标在 LEFT_VERSION 和 RIGHT_VERSION 之间被删除';
 $messages['help_translating'] = '在Translatewiki.net帮助翻译';
 $messages['start_here'] = '从这里开始搜索';
-$messages['too_much_versions'] = '您已到达您的 __VERSIONLIMIT__ 个版本的查询限制。请在 __WAITMINUTES__ 分钟后再试，或切换到二进制搜索。不便之处，敬请原谅。';
+$messages['too_much_versions'] = '您已到达您的 __VERSIONLIMIT__ 个版本的查询限制。请在 __WAITMINUTES__ 分钟后再试，或切换到二分法搜索。不便之处，敬请原谅。';
 $messages['not_found_at_all'] = '完全找不到您的搜寻词。请检查设置后重试。';
