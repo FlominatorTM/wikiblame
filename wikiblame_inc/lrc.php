0150688fe4e56f136e4947d6a5ad16561cda139a
Siebrand Mazeland <Siebrand@users.noreply.github.com>
2013-12-23 20:38:01 +0000

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/lrc.php
@@ -0,0 +1,44 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** لوری (لوری)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Mogoeilor
+ */
+
+$messages['January'] = 'جانويه';
+$messages['February'] = 'فوريه';
+$messages['March'] = 'مارس';
+$messages['April'] = 'آوريل';
+$messages['May'] = 'ما';
+$messages['June'] = 'جوئن';
+$messages['July'] = 'جولا';
+$messages['August'] = 'اگوست';
+$messages['September'] = 'سپتامر';
+$messages['October'] = 'اكتور';
+$messages['November'] = 'نوامر';
+$messages['December'] = 'دسامر';
+$messages['ui_lang'] = 'زون نه نشو بیئه';
+$messages['lang'] = 'زون';
+$messages['project'] = 'پروجه';
+$messages['article'] = 'بلگه';
+$messages['needle'] = 'پی جوری سی';
+$messages['start_date'] = 'زمان شرو';
+$messages['date_format'] = 'ما،روز،سال';
+$messages['order'] = 'دستور';
+$messages['newest_first'] = 'اول تازه ترا';
+$messages['search_method'] = 'روشت پی جوری';
+$messages['binary'] = 'دوتایی';
+$messages['linear'] = 'خطی';
+$messages['start'] = 'شرو كو';
+$messages['reset'] = 'د نو شرو كردن';
+$messages['contact'] = 'تماس';
+$messages['please_wait'] = 'خواهشن تيه وه ره بمونيت...';
+$messages['start_here'] = 'ایچه نه بگردیت';
