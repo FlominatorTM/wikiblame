689dcaa69794881cdd83182b9ae71c7c8c5564b7
flominator <flominator@gmx.net>
2013-12-29 09:53:48 +0000

    initial commit for foto_range.php

---
+++ next_inc/OfferPage.php
@@ -0,0 +1,123 @@
+<?php
+
+class OfferPage
+{
+	public $userOffers;
+	
+	function __construct($page) 
+	{
+		global $server, $messages;
+		//echo "page=".$page;
+		
+		$ConfigDir = 'next_inc/proj';
+		$ConfigFile = $ConfigDir . '/' . $server . '.php';
+		if(!file_exists($ConfigFile))
+		{
+			die(str_replace('_PROJECT_', $server, $messages['proj_not_supported']));
+		}
+		else
+		{
+			include($ConfigFile);
+		}
+
+		$page_src = removeheaders(get_request($server, $page, true ));
+
+		//print_debug("page_src=".$page_src);
+		//print_debug("<hr><hr>");
+		$page_parts = explode('{{'.$TemplateName, $page_src);
+
+		foreach($page_parts as $template)
+		{
+			//print_debug("<hr>");
+			//print_debug("<h1>template</h1>$template");
+
+			$usr = new OfferingUser($this->extractTemplateParameter($template, $TemplateUser));
+			
+			$location = new GeoLocation($this->extractTemplateParameter($template, $TemplateLocation));
+			$usr->SetLocation($location);
+			print_debug("<b>".$location->name."</b>");
+			$range = $this->extractTemplateParameter($template, $TemplateRange);
+			// print_debug("<b>".$range."</b><br />");
+			$usr->SetRange($range);
+			
+			//print_debug($usr->ToString());
+
+			if($usr->IsValid())
+			{
+				print_debug("user $usr->name is valid: ".$usr->ToString());
+				$this->userOffers[] = $usr;
+			}
+			echo $coordinates_xml;
+		}
+	}
+	
+	function ListUsersToRequest($requested)
+	{
+		global $messages;
+		$locTo = new GeoLocation($requested);
+		if($locTo->IsValid())
+		{
+			print_debug("$locTo->ToString()=>" . $locTo->ToString());
+			foreach($this->userOffers as $usr)
+			{
+				$usr->SetDistance($locTo);
+			}
+			
+			usort($this->userOffers , array("OfferingUser", "CompareDistance"));
+			
+			foreach($this->userOffers as $usr)
+			{
+				$resLine = $usr->LinkToUser() . "  (" . sprintf("%01.1f",$usr->distance)  . " km)";
+				if($usr->IsInRange())
+				{
+					echo "<b>$resLine</b>";
+				}
+				else
+				{
+					echo "$resLine";
+				}
+				
+				
+				echo "<br>";
+			}
+		}
+		else
+		{
+			echo str_replace('_LOCATION_', $requested, $messages['no_coordinates']);
+		}
+	}
+	
+	private function extractTemplateParameter($template_text, $parameter)
+	{
+		$str_return = "";
+		if(stristr($template_text, $parameter))
+		{
+			$index_of_label = strpos($template_text, $parameter);
+			$index_of_equal_sign = strpos($template_text, "=", $index_of_label);
+			$index_of_pipe_sign = strpos($template_text, "|" , $index_of_label);
+			$index_of_template_end_sign = strpos($template_text, "}" , $index_of_label);
+			
+			$index_of_sign_after_parameter = $index_of_pipe_sign ;
+			
+			
+			if($index_of_sign_after_parameter == 0  
+			|| $index_of_template_end_sign < $index_of_pipe_sign)
+			{
+				$index_of_sign_after_parameter  = $index_of_template_end_sign;
+			}
+			$length_of_location = $index_of_sign_after_parameter - $index_of_equal_sign -2;
+			
+			// print_debug("location_label = $parameter");
+			// print_debug("index_of_label = $index_of_label");
+			// print_debug("index_of_equal_sign = $index_of_equal_sign");
+			// print_debug("index_of_pipe_sign = $index_of_pipe_sign");
+			// print_debug("index_of_template_end_sign = $index_of_template_end_sign");
+			// print_debug("index_of_sign_after_parameter= index_of_sign_after_parameter");
+			
+			// print_debug("length_of_location = $length_of_location");
+			
+			$str_return =  substr($template_text, $index_of_equal_sign+1, $length_of_location);
+		}
+		return $str_return ;
+	}
+}
---
+++ next_inc/lang/en.php
@@ -0,0 +1,12 @@
+<?php
+
+//template
+$messages['template_offer'] = 'Bilderangebot';
+$messages['template_user'] = 'Benutzer';
+$messages['template_location'] = 'Standort';
+$messages['template_range'] = 'Aktionsradius';
+
+//result
+$messages['distance_to'] = 'Distance to coordinates from [[_ARTICLE_TO_]] ';
+$messages['no_coordinates'] = 'location _LOCATION_ is invalid/does not have coordinates';
+$messages['proj_not_supported'] = 'project _PROJECT_ is currently not supported, please contact :de:user:Flominator for details';
