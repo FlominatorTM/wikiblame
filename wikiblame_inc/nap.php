c780ddee38dba387ea2dad1a6833abb4f6a58f1a
Siebrand Mazeland <Siebrand@users.noreply.github.com>
2014-01-06 11:33:43 +0000

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/nap.php
@@ -0,0 +1,53 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Neapolitan (Napulitano)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Chelin
+ */
+
+$messages['January'] = 'Jennaro';
+$messages['February'] = 'Frevaro';
+$messages['March'] = 'Màrzo';
+$messages['April'] = 'Abbrile';
+$messages['May'] = 'Màjo';
+$messages['June'] = 'Giùgno';
+$messages['July'] = 'Luglio';
+$messages['August'] = 'Aùsto';
+$messages['September'] = 'Settembre';
+$messages['October'] = 'Ottovre';
+$messages['November'] = 'Nuvembre';
+$messages['December'] = 'Dicembre';
+$messages['ui_lang'] = 'Lengua \'e visualizzazione';
+$messages['lang'] = 'Lengua';
+$messages['project'] = 'Pruggietto';
+$messages['article'] = 'Paggena';
+$messages['needle'] = 'Circa \'a stringa';
+$messages['skipversions'] = 'Esclude sempe x versione';
+$messages['ignorefirst'] = 'Ignora \'e primme x versione';
+$messages['limit'] = 'Versione \'a cuntrolla';
+$messages['start_date'] = 'Data inizzio';
+$messages['date_format'] = 'DD MM YYYY';
+$messages['order'] = 'Ordene';
+$messages['newest_first'] = 'primma \'e cchiù recente';
+$messages['oldest_first'] = 'primma \'e meno recente';
+$messages['binary_search_inverse'] = 'Circa \'a rimozzione d"o testo (solo binario)';
+$messages['search_method'] = 'Metodo \'e ricerca';
+$messages['binary'] = 'binario';
+$messages['linear'] = 'lineare';
+$messages['interpolated'] = 'binario (cchiù veloce cu molte versione \'a cuntrolla)';
+$messages['ignore_minors'] = 'ignora \'e cagnamiente minore (sperimentale)';
+$messages['force_wikitags'] = 'forza \'a ricerca \'e wikitext';
+$messages['start'] = 'Inizzia';
+$messages['reset'] = 'Reimposta';
+$messages['manual'] = 'Guida';
+$messages['contact'] = 'Cuntatto';
+$messages['please_wait'] = 'Aspetta ppe favore …';
+$messages['start_here'] = 'Circa \'e ccà';
