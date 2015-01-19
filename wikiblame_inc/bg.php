1dc6b8c428fc88d5a50ca9a59d5b5cf63d40fe5c
siebrand <siebrand@f05a5d5e-15d0-4e17-8f62-caf47b77aea6>
2015-01-19 08:35:33 +0000

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/bg.php
+++ wikiblame_inc/bg.php
@@ -11,6 +11,7 @@
  * @file
  *
  * @author DCLXVI
+ * @author Vodnokon4e
  */
 
 $messages['January'] = 'януари';
@@ -25,14 +26,38 @@ $messages['September'] = 'септември';
 $messages['October'] = 'октомври';
 $messages['November'] = 'ноември';
 $messages['December'] = 'декември';
+$messages['ui_lang'] = 'Език на изгледа';
 $messages['lang'] = 'Език';
 $messages['project'] = 'Проект';
 $messages['article'] = 'Страница';
 $messages['needle'] = 'Търсене за';
+$messages['skipversions'] = 'Пропускане на х версии';
+$messages['ignorefirst'] = 'Игнориране на първите х версии';
 $messages['limit'] = 'Версии за проверяване';
 $messages['start_date'] = 'Начална дата';
+$messages['date_format'] = 'MM DD, YYYY';
 $messages['order'] = 'Подредба';
 $messages['newest_first'] = 'първо най-новите';
 $messages['oldest_first'] = 'първо най-старите';
+$messages['binary_search_inverse'] = 'Търси изтриване на текст (само двоичен)';
 $messages['search_method'] = 'Метод на търсене';
+$messages['binary'] = 'Двоичен';
+$messages['linear'] = 'Линеен';
+$messages['interpolated'] = 'двоичен (по-бърз с повече версии)';
+$messages['ignore_minors'] = 'Игнориране на малки промени (експериментално)';
+$messages['start'] = 'Начало';
+$messages['reset'] = 'Изчистване';
+$messages['manual'] = 'Упътване';
+$messages['contact'] = 'Контакт';
+$messages['get_less_versions'] = 'Търсенето може да зададе __NUMREVISIONS__ версии едновременно. С цел защита на сървъра, е разрешено само __ALLOWEDREVISIONS__ заявки на търсене. Моля, променете настройките или преминете в двоичен режим на търсене!';
+$messages['wrong_skips'] = 'Неправилни настройки: Ако първите __VERSIONSTOSKIP__ версии са пропуснати, то нито едно търсене за __VERSIONSTOSEARCH__ версии няма да бъде изпълнено.';
+$messages['no_differences'] = 'Не са открити разлики между избраните версии.';
+$messages['earlier_versions_available'] = 'Вероятно има и по-стари версии.';
+$messages['please_wait'] = 'Моля, изчакайте...';
+$messages['binary_test'] = 'Сравнение на разликите в _FIRSTDATEVERSION_ между _FIRSTNUMBER_ и _SECONDNUMBER_ по време на прехода от _SOURCENUMBER_:';
+$messages['binary_enough'] = 'Извършени са достатъчно опити, историята на статията е твърде объркана. Моля, опитайте се да промените някои от настройките.';
+$messages['insertion_found'] = 'Открито е добавяне между LEFT_VERSION and RIGHT_VERSION';
+$messages['deletion_found'] = 'Открито е изтриване между LEFT_VERSION и RIGHT_VERSION';
 $messages['help_translating'] = 'Помогнете с превода на translatewiki.net';
+$messages['start_here'] = 'Започни търсене от тук';
+$messages['not_found_at_all'] = 'Вашето търсене не откри резултати. Проверете настройките си и опитайте отново.';
