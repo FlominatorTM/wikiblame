d1b75a75987bb9c15ba308cbe29fd7066402a7ad
Siebrand Mazeland <siebrand@kitano.nl>
2015-06-22 11:58:48 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/bcl.php
@@ -0,0 +1,45 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Bikol Central (Bikol Central)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Geopoet
+ */
+
+$messages['January'] = 'Enero';
+$messages['February'] = 'Pebrero';
+$messages['March'] = 'Marso';
+$messages['April'] = 'Abril';
+$messages['May'] = 'Mayo';
+$messages['June'] = 'Hunyo';
+$messages['July'] = 'Hulyo';
+$messages['August'] = 'Agosto';
+$messages['September'] = 'Septiyembre';
+$messages['October'] = 'Oktobre';
+$messages['November'] = 'Nobyembre';
+$messages['December'] = 'Disyembre';
+$messages['ui_lang'] = 'Ipahiling an lengguwahe';
+$messages['lang'] = 'Lengguwahe';
+$messages['project'] = 'Proyekto';
+$messages['article'] = 'Pahina';
+$messages['needle'] = 'Maghanap kan';
+$messages['skipversions'] = 'Pirmeng laktawan an x na mga bersyon';
+$messages['ignorefirst'] = 'Dae pansinon an enot na mga x bersyon';
+$messages['limit'] = 'Mga bersyon na i-tsek';
+$messages['start_date'] = 'Petsa sa pagpoon';
+$messages['date_format'] = 'BB AA, TTTT';
+$messages['order'] = 'Pasurunudan';
+$messages['newest_first'] = 'enot an pinakabaguhon';
+$messages['oldest_first'] = 'enot an pinakalumaon';
+$messages['binary_search_inverse'] = 'Hanapon para sa pagtangkas kan teksto (binaryo sana)';
+$messages['search_method'] = 'Paagi sa paghanap';
+$messages['binary'] = 'binaryo';
+$messages['linear'] = 'linaryo';
+$messages['interpolated'] = 'binaryo (marikas kaiba an iba pang mga bersyon)';
