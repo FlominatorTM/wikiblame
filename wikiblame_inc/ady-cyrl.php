0917d2feaab063c8aa79a43578479a8bbc47663e
Siebrand Mazeland <siebrand@kitano.nl>
2015-07-14 16:26:37 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/ady-cyrl.php
+++ wikiblame_inc/ady-cyrl.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** Адыгэбзэ (Адыгэбзэ)
+/** Адыгабзэ (Адыгабзэ)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
