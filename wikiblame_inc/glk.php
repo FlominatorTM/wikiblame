3f15d20f48701d015f91901e941e0ca54727924f
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-01-25 07:43:42 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/glk.php
+++ wikiblame_inc/glk.php
@@ -13,5 +13,24 @@
  * @author شیخ
  */
 
+$messages['January'] = 'ژانویه';
+$messages['February'] = 'فئوريه';
+$messages['March'] = 'مارس';
+$messages['April'] = 'آوريل';
+$messages['May'] = 'مئی';
+$messages['June'] = 'جۊن';
+$messages['July'] = 'جۊلاى';
+$messages['August'] = 'آگؤست';
+$messages['September'] = 'سپتامبر';
+$messages['October'] = 'اؤکتؤبر';
+$messages['November'] = 'نؤوامبر';
+$messages['December'] = 'دسامبر';
+$messages['ui_lang'] = 'زوانه دئن';
+$messages['lang'] = 'زوان';
+$messages['project'] = 'پؤرؤژه';
+$messages['article'] = 'ولگ';
+$messages['needle'] = 'وامج';
+$messages['skipversions'] = 'هميشک xˇ نۊسخه\'نه وابدي';
+$messages['contact'] = 'تماس';
 $messages['please_wait'] = 'خأهشن رافا بئسين...';
-$messages['start_here'] = 'ائره جي وامۊتن';
+$messages['start_here'] = 'ائره جي وامج';
