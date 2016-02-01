2819b5760ff3b5812cc9e8d0662ae7eb20fbe9ed
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-02-01 09:32:51 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/fur.php
@@ -0,0 +1,73 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Friulian (furlan)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Tocaibon
+ */
+
+$messages['January'] = 'Zenâr';
+$messages['February'] = 'Fevrâr';
+$messages['March'] = 'Març';
+$messages['April'] = 'Avrîl';
+$messages['May'] = 'Mai';
+$messages['June'] = 'Jugn';
+$messages['July'] = 'Lui';
+$messages['August'] = 'Avost';
+$messages['September'] = 'Setembar';
+$messages['October'] = 'Otubar';
+$messages['November'] = 'Novembar';
+$messages['December'] = 'Dicembar';
+$messages['ui_lang'] = 'Lenghe di visualizazion';
+$messages['lang'] = 'Lenghe';
+$messages['project'] = 'Progjet:';
+$messages['article'] = 'Pagjine';
+$messages['needle'] = 'Cîr';
+$messages['skipversions'] = 'Salte simpri x versions';
+$messages['ignorefirst'] = 'Ignore li primis x versions';
+$messages['limit'] = 'Version di controlâ';
+$messages['start_date'] = 'Date iniziâl';
+$messages['date_format'] = 'MM DD, AAAA';
+$messages['order'] = 'Ordenament';
+$messages['newest_first'] = 'prime lis plui gnovis';
+$messages['oldest_first'] = 'prime lis plui vecjis';
+$messages['binary_search_inverse'] = 'Cîr la rimozion dal test (dome binari)';
+$messages['search_method'] = 'Mût di ricercje';
+$messages['binary'] = 'ninari';
+$messages['linear'] = 'lineâr';
+$messages['interpolated'] = 'binari (plui veloç con tantis versions di controlâ)';
+$messages['ignore_minors'] = 'ignore lis piçulis modifichis (sperimentâl)';
+$messages['force_wikitags'] = 'Oblighe la ricercje di wikitext';
+$messages['start'] = 'Comence';
+$messages['reset'] = 'Reimpueste';
+$messages['manual'] = 'Manuâl';
+$messages['contact'] = 'Contat';
+$messages['get_less_versions'] = 'La ricercje a podarés controlâ __NUMREVISIONS__ revisions ae volte. Par protezi il server, al è pussibil controlâ dome __ALLOWEDREVISIONS__ revisions par volte. Modifiche lis impostazions, o dopre il metodi di ricerche binari.';
+$messages['wrong_skips'] = 'Configurazion sbaliade: se lis primis __VERSIONSTOSKIP__
+versions a vegnin saltadis, alore nessune des __VERSIONSTOSEARCH__ versions di cirî a saran processadis.';
+$messages['search_in_progress_text'] = 'La cronologje di _ARTICLELINK_ a ven cumò analizade te ricerche <b>_NEEDLE_</b> come test sempliç';
+$messages['search_in_progress_wikitags'] = 'La cronologje di _ARTICLELINK_ a ven cumò analizade te ricerche <b>_NEEDLE_</b> come test wiki';
+$messages['no_differences'] = 'Nissune difarence cjatade';
+$messages['first_version'] = 'La modifiche e je stade te prime o te ultime version?';
+$messages['first_version_present'] = '__NEEDLE__ al jere za presint inte prime revision dal __REVISIONLINK__.';
+$messages['earlier_versions_available'] = 'A varessin di sei revisions precedentis';
+$messages['execution_time'] = 'Timp di esecuzion: _EXECUTIONTIME_ seconts';
+$messages['versions_found'] = '_NUMBEROFVERSIONS_ revisions cjatadis';
+$messages['please_wait'] = 'Par plasê spiete.....';
+$messages['binary_test'] = 'Confront des difarencis inte revision dal _FIRSTDATEVERSION_ fra la revision _FIRSTNUMBER_ e la _SECONDNUMBER_ intant che a ven de revision _SOURCENUMBER_:';
+$messages['dead_end'] = 'Cjatât un pont muart (probabilmentri causât da une cancelazion in rollback o da une dispute fra utents edit war)';
+$messages['once_more'] = 'Ancjemò une volte, cun passion';
+$messages['binary_enough'] = 'Fats un numar suficient di tentatîfs, la cronologjie de vô e je dut câs ancjemo masse incasinade, prove par plasê a cambiâ culachi impostazion.';
+$messages['insertion_found'] = 'Inseriment de paraule cjatât tra la revision dal LEFT_VERSION e chê dal RIGHT_VERSION';
+$messages['deletion_found'] = 'Cancelazion cjatâde tra la revision dal LEFT_VERSION e chê dal RIGHT_VERSION';
+$messages['help_translating'] = 'Jude a traduzi su translatewiki.net';
+$messages['start_here'] = 'Cîr di ca vie';
+$messages['too_much_versions'] = 'Tu sês rivât/rivade al limit massim  di __VERSIONLIMIT__ domandadis. Par plasê torne a provâ fra __WAITMINUTES__ minéts o passe ael metodi di ricerche binari. Nus displâs pal inconvenient.';
+$messages['not_found_at_all'] = 'La peraule cirude no je stade cjatade. Controle lis impostazions e torne a provâ.';
