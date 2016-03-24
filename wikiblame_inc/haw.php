9a69d9106bdc3e8b030949dea92df4f298e4b6d0
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-03-24 07:29:00 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/haw.php
+++ wikiblame_inc/haw.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** Hawaiian (Hawai`i)
+/** Hawaiian (Hawaiʻi)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
