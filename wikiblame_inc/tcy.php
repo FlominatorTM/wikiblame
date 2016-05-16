b5e7568746a3e870f901e739242c3b8493f607f8
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-05-16 07:39:40 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/tcy.php
@@ -0,0 +1,39 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Tulu (ತುಳು)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author VASANTH S.N.
+ */
+
+$messages['January'] = 'ಜನವರಿ';
+$messages['February'] = 'ಪೆಬ್ರವರಿ';
+$messages['March'] = 'ಮಾರ್ಚಿ';
+$messages['April'] = 'ಎಪ್ರಿಲ್';
+$messages['May'] = 'ಮೇ';
+$messages['June'] = 'ಜೂನ್';
+$messages['July'] = 'ಜುಲಾಯಿ';
+$messages['August'] = 'ಆಗೋಸ್ಟು';
+$messages['September'] = 'ಸಪ್ಟಂಬರೊ';
+$messages['October'] = 'ಅಕ್ಟೋಬರ';
+$messages['November'] = 'ನವಂಬರೊ';
+$messages['December'] = 'ದಸಂಬರೊ';
+$messages['ui_lang'] = 'ಪ್ರದರ್ಶಕ ಭಾಷೆ';
+$messages['lang'] = 'ಭಾಸೆ';
+$messages['project'] = 'ಯೋಜನೆಲು';
+$messages['article'] = 'ಪುಟೊ';
+$messages['needle'] = 'ನಾಡ್ಲೆ...';
+$messages['order'] = 'ಕ್ರಮ';
+$messages['binary'] = 'ದ್ವಿಮಾನ';
+$messages['linear'] = 'ರೇಖಾತ್ಮಕ';
+$messages['start'] = 'ಸುರು';
+$messages['reset'] = 'ಪಿರ ಸರಿಮಾನ್ಪಿ';
+$messages['manual'] = 'ಕೈಪಿಡಿ';
+$messages['contact'] = 'ಸಂಪರ್ಕ';
