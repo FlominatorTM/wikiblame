b6257679d3f8d0a05a8c03f6a922fd7b539f54eb
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-05-19 09:39:10 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/vo.php
@@ -0,0 +1,30 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Volapük (Volapük)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Robin van der Vliet
+ */
+
+$messages['January'] = 'yanul';
+$messages['February'] = 'febul';
+$messages['March'] = 'mäzul';
+$messages['April'] = 'prilul';
+$messages['May'] = 'mayul';
+$messages['June'] = 'yunul';
+$messages['July'] = 'yulul';
+$messages['August'] = 'gustul';
+$messages['September'] = 'setul';
+$messages['October'] = 'tobul';
+$messages['November'] = 'novul';
+$messages['December'] = 'dekul';
+$messages['lang'] = 'Pük';
+$messages['project'] = 'Proyek';
+$messages['article'] = 'Pad';
