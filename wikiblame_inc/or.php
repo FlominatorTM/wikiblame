17bc681f1cc81393880f359b11af25cf8be83848
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-11-03 08:19:56 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/or.php
+++ wikiblame_inc/or.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** Oriya (ଓଡ଼ିଆ)
+/** Odia (ଓଡ଼ିଆ)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
