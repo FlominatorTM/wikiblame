c55ff57667d82098a04dff8f79ae342c99d086f6
Siebrand Mazeland <siebrand@kitano.nl>
2016-11-04 21:44:31 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/be.php
+++ wikiblame_inc/be.php
@@ -10,6 +10,7 @@
  * @ingroup Language
  * @file
  *
+ * @author Mechanizatar
  * @author SamGold
  */
 
@@ -40,4 +41,26 @@ $messages['newest_first'] = 'спачатку новыя';
 $messages['oldest_first'] = 'спачатку старыя';
 $messages['binary_search_inverse'] = 'Пошук выдалення тэксту (толькі дваічны)';
 $messages['search_method'] = 'Метад пошуку';
+$messages['binary'] = 'бінарны';
+$messages['linear'] = 'лінейны';
+$messages['interpolated'] = 'бінарны (хутчэй, калі шмат версіяў)';
+$messages['ignore_minors'] = 'Ігнараваць нязначныя змены (экспэрымэнтальна)';
+$messages['force_wikitags'] = 'Шукаць па вікі-тэксце';
 $messages['start'] = 'Запусціць';
+$messages['reset'] = 'Скінуць';
+$messages['manual'] = 'Інструкцыя';
+$messages['contact'] = 'Кантактная інфармацыя';
+$messages['no_differences'] = 'У шукаемых версіях адрозненняў ня знойдзена.';
+$messages['inverse_restart'] = 'Устаўкі ці выдалення не знойдзена. Ці не было пошукавае слова ўстаўлена пазней?';
+$messages['first_version'] = 'Змена павінна адбыцца ў першай ці апошняй версіі?';
+$messages['earlier_versions_available'] = 'Магчыма, існуюць больш старыя версіі.';
+$messages['please_wait'] = 'Калі ласка, пачакайце…';
+$messages['dead_end'] = 'Перашкаджае тупік (верагодна, выкліканы адкатамі ці вайной правак)';
+$messages['once_more'] = 'Яшчэ раз, з пачуццём:';
+$messages['binary_enough'] = 'Выканана дастаткова спробаў, гісторыя артыкула зусім заблыталася. Калі ласка, паспрабуйце змяніць некаторыя параметры.';
+$messages['insertion_found'] = 'Ёсць устаўка паміж LEFT_VERSION і RIGHT_VERSION';
+$messages['deletion_found'] = 'Ёсць выдаленне паміж LEFT_VERSION і RIGHT_VERSION';
+$messages['here'] = 'тут';
+$messages['help_translating'] = 'Дапамагчы з перакладам на translatewiki.net';
+$messages['start_here'] = 'Пошук адсюль';
+$messages['not_found_at_all'] = 'Па вашым пошукавым запыце нічога не знойдзена. Праверце правільнасць наладаў і паспрабуйце яшчэ раз.';
