9d99df33c0a6c86c9f08b30f42abd3450727fa6f
translatewiki.net <l10n-bot@translatewiki.net>
2017-02-16 09:38:07 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/ar.php
+++ wikiblame_inc/ar.php
@@ -10,6 +10,7 @@
  * @ingroup Language
  * @file
  *
+ * @author Aboulouei1
  * @author Ciphers
  * @author DRIHEM
  * @author Hhaboh162002
@@ -45,7 +46,7 @@ $messages['skipversions'] = 'تخطَّ دائما النسخ س';
 $messages['ignorefirst'] = 'أهمل النسخة س الأولى';
 $messages['limit'] = 'نسخ للمراجعة';
 $messages['start_date'] = 'تاريخ البداية';
-$messages['date_format'] = 'DD MM YYYY';
+$messages['date_format'] = ' MM DD, YYYY';
 $messages['order'] = 'الترتيب';
 $messages['newest_first'] = 'الآخر أولا';
 $messages['oldest_first'] = 'الأقدم أولا';
@@ -54,32 +55,42 @@ $messages['search_method'] = 'طريقة البحث';
 $messages['binary'] = 'ثنائي';
 $messages['binary_in_wp'] = 'http://ar.wikipedia.org/wiki/%D8%AE%D9%88%D8%A7%D8%B1%D8%B2%D9%85%D9%8A%D8%A9_%D8%A7%D9%84%D8%A8%D8%AD%D8%AB_%D8%A7%D9%84%D8%AB%D9%86%D8%A7%D8%A6%D9%8A';
 $messages['linear'] = 'خطيّة';
-$messages['interpolated'] = 'ثنائي (أسرع بنسخ أكثر)';
+$messages['interpolated'] = 'ثنائي (أسرع بنسخ أكثر)
+الثنائي (أسرع مع المزيد من الإصدارات)';
 $messages['ignore_minors'] = 'تجاهل التغييرات الثانوية (تجريبية)';
 $messages['force_wikitags'] = 'إجبار البحث على نص ويكي';
 $messages['start'] = 'ابدأ';
 $messages['reset'] = 'أعد الضبط';
 $messages['manual'] = 'دليل الاستعمال';
 $messages['contact'] = 'اتصل';
-$messages['get_less_versions'] = 'يمكن لبحثك أن يظم __NUMREVISIONS__ تعديلات في وقت واحد. يسمح لك بالبحث عن __ALLOWEDREVISIONS__ لكل مكالمة، وذلك لحماية الخادم. رجاءً قم بتعديل الإعدادات أو حول طريقة البحث إلى ثنائي!';
+$messages['get_less_versions'] = 'يمكن لبحثك أن يظم __NUMREVISIONS__ تعديلات في وقت واحد. يسمح لك بالبحث عن __ALLOWEDREVISIONS__ لكل مكالمة، وذلك لحماية الخادم. رجاءً قم بتعديل الإعدادات أو حول طريقة البحث إلى ثنائي!
+يمكن لبحثك أن يظم  __NUMREVISIONS__ في وقت واحد. من أجل حماية الخادم، ويسمح لك فقط للاستعلام عن __ALLOWEDREVISIONS__ لكل مكالمة. الرجاء قم بتغيير إعدادات أو تبديل طريقة البحث الثنائي!';
 $messages['wrong_skips'] = 'إعدادات غير صحيحة: لو أن أول نسخ __VERSIONSTOSKIP__ تم تجاوزها, إذا فلا شيء من نسخ __VERSIONSTOSEARCH__ للبحث ستتم معالجتها.';
-$messages['search_in_progress_text'] = 'تاريخ النسخةهذه  _ARTICLELINK_ يجري البحث عنها <b>_NEEDLE_</b> كنص عادي';
+$messages['search_in_progress_text'] = 'تاريخ هذه النسخة  _ARTICLELINK_ يجري البحث عنه <b>_NEEDLE_</b/> كنص عادي';
 $messages['search_in_progress_wikitags'] = 'تاريخ النسخة هذه  _ARTICLELINK_ يجري البحث عنها <b>_NEEDLE_</b> كنص ويكي';
 $messages['no_differences'] = 'لا توجد اختلافات في البحث في التنقيحات.';
-$messages['first_version'] = 'يجب أن يكون قد حدث تغيير في التنقيح الأول أو الأخير؟';
+$messages['inverse_restart'] = 'لا وجود لإدراج أو إزالة قد يتم إدخال مصطلح البحث في وقت لاحق ؟';
+$messages['first_version'] = 'يجب أن يكون قد حدث تغيير في التنقيح الأول أو الأخير؟
+التغيير يجب أن يحدث في أول أو آخر التنقيح ؟';
 $messages['first_version_present'] = '__NEEDLE__ موجود بالفعل في أقدم مراجعة بحثت عنها منذ __REVISIONLINK__ .';
 $messages['earlier_versions_available'] = 'يحتمل أن توجد مراجعات أقدم.';
-$messages['execution_time'] = 'وقت التنفيذ: _EXECUTIONTIME_ ثانية';
+$messages['execution_time'] = 'وقت التنفيذ: _EXECUTIONTIME_ ثواني';
 $messages['versions_found'] = '_NUMBEROFVERSIONS_ نسخ موجودة';
 $messages['please_wait'] = 'الرجاء الانتظار...';
 $messages['binary_test'] = 'مقارنة الاختلافات في _FIRSTDATEVERSION_ بين _FIRSTNUMBER_ و _SECONDNUMBER_ حتى يحصل عليه من _SOURCENUMBER_:';
 $messages['dead_end'] = 'وصلت إلى طريق مسدود (سببه على الأرجح رجوع أو حروب تحريرية)';
-$messages['once_more'] = 'مرة أخرى، مع الشعور :';
+$messages['once_more'] = 'مرة أخرى، بالمشاعر :';
 $messages['binary_enough'] = 'قمت بتنفيذ أكبر عدد من المحاولات، إن التاريخ المادة قد أفسد تماما، رجاء حاول تغيير بعض الإعدادات.';
-$messages['insertion_found'] = 'وجد الحذف بين LEFT_VERSION و RIGHT_VERSION';
+$messages['insertion_found'] = 'وجد الحذف بين LEFT_VERSION و RIGHT_VERSION
+
+وجد الإدراج بين LEFT_VERSION وRIGHT_VERSION';
 $messages['deletion_found'] = 'وجد الحذف بين LEFT_VERSION و RIGHT_VERSION';
 $messages['here'] = 'هنا';
 $messages['help_translating'] = 'ساعد في الترجمة عبر ترانسليت ويكي دوت نت';
 $messages['start_here'] = 'ابحث من هنا';
-$messages['too_much_versions'] = 'لقد وصلت إلى حد الاستعلام المخصص بعدد __VERSIONLIMIT__ إصدار. يرجى المحاولة مرة أخرى في غضون __WAITMINUTES__دقائق أو التبديل للبحث الثنائي. نأسف للإزعاج.';
-$messages['not_found_at_all'] = 'لم نتمكن من العثور على المصطلح الذي تبحث عنه على الإطلاق. تفقد الإعدادات وأعد المحاولة من جديد.';
+$messages['too_much_versions'] = 'لقد وصلت إلى حد الاستعلام المخصص بعدد __VERSIONLIMIT__ إصدار. يرجى المحاولة مرة أخرى في غضون __WAITMINUTES__دقائق أو التبديل للبحث الثنائي. نأسف للإزعاج.
+
+لقد وصلت إلى حد الاستعلام الخاص بك من الإصدارات __VERSIONLIMIT__. يرجى المحاولة مرة أخرى في غضون دقائق __WAITMINUTES__ أو تحول إلى البحث الثنائي. آسف للإزعاج.';
+$messages['not_found_at_all'] = 'لم نتمكن من العثور على المصطلح الذي تبحث عنه على الإطلاق. تفقد الإعدادات وأعد المحاولة من جديد.
+
+لم يتم العثور على المصطلح الذي تبحث عنه على الإطلاق. تحقق من الإعدادات وحاول مرة أخرى.';
