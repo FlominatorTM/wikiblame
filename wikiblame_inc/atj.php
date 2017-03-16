1483e79dd53667f63808b88ac3d52d9707f7c912
translatewiki.net <l10n-bot@translatewiki.net>
2017-03-16 08:20:58 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/atj.php
@@ -0,0 +1,29 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Atikamekw (Atikamekw)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Benoit Rochon
+ */
+
+$messages['January'] = 'Kenositc pisimw';
+$messages['February'] = 'Akokatcic pisimw';
+$messages['March'] = 'Nikikw pisimw';
+$messages['April'] = 'Ka wasikototc pisimw';
+$messages['May'] = 'Wapikon pisimw';
+$messages['June'] = 'Otehimin pisimw';
+$messages['July'] = 'Mikomini pisimw';
+$messages['August'] = 'Otatakon pisimw';
+$messages['September'] = 'Kakone pisimw';
+$messages['October'] = 'Namekosi pisimw';
+$messages['November'] = 'Atikamekw pisimw';
+$messages['December'] = 'Pitcipipon pisimw';
+$messages['project'] = 'Tacikatek';
+$messages['article'] = 'Masinhikan';
