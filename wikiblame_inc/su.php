c4b4b217d39327264da942afba38c92963222c2d
translatewiki.net <l10n-bot@translatewiki.net>
2017-04-17 09:43:49 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/su.php
+++ wikiblame_inc/su.php
@@ -11,6 +11,7 @@
  * @file
  *
  * @author Kandar
+ * @author Uchup19
  */
 
 $messages['January'] = 'Januari';
@@ -30,4 +31,36 @@ $messages['lang'] = 'Basa';
 $messages['project'] = 'Proyék';
 $messages['article'] = 'Kaca';
 $messages['needle'] = 'Paluruh';
-$messages['skipversions'] = 'Salawasna liwat vérsi x';
+$messages['skipversions'] = 'Liwatkeun baé vérsi x pikeun saterusna';
+$messages['ignorefirst'] = 'Antepkeun x vérsi mimiti';
+$messages['limit'] = 'Vérsi pariksaeun';
+$messages['start_date'] = 'Tanggal ngamimitian';
+$messages['date_format'] = 'DD MM YYYY';
+$messages['order'] = 'Runtuyan';
+$messages['newest_first'] = 'Mimiti nu panganyarna';
+$messages['oldest_first'] = 'Mimiti nu pangheubeulna';
+$messages['binary_search_inverse'] = 'Ilikan pamupusan téks (binér hungkul)';
+$messages['search_method'] = 'Métodeu pamaluruhan';
+$messages['binary'] = 'binér';
+$messages['linear'] = 'liniér';
+$messages['interpolated'] = 'binér (leuwih gancang ku vérsi nu leuwih loba)';
+$messages['ignore_minors'] = 'antepkeun parobahan minor (ékspérimental)';
+$messages['force_wikitags'] = 'Paksa pamaluruhan téks wiki';
+$messages['start'] = 'Mimitian';
+$messages['reset'] = 'Rését';
+$messages['manual'] = 'Manual';
+$messages['contact'] = 'Kontak';
+$messages['get_less_versions'] = 'Pamaluruhan anjeun bisa ménta __NUMREVISIONS__ révisi dina sawaktu reujeung. Pikeun nangtayungan sérver, anjeun ngan digaékeun pikeun ménta __ALLOWEDREVISIONS__ per pamundutan. Pèk robah baé pangaturan atawa pindah kana métodeu pamaluruhan binér!';
+$messages['wrong_skips'] = 'Kasalahan setélan: Mun __VERSIONSTOSKIP__ vérsi diliwatan, mangka moal aya deui tina __VERSIONSTOSEARCH__ vérsi anu dipaluruh nu bakal diprosés.';
+$messages['search_in_progress_wikitags'] = 'Jujutan vérsi tina _ARTICLELINK_ keur ditéangan pikeun <b>_NEEDLE_</b> minangka téks wiki';
+$messages['no_differences'] = 'Teu manggihan parobahan dina pamaluruhan révisi.';
+$messages['earlier_versions_available'] = 'Meureun aya vérsi nu leuwih heubeul.';
+$messages['execution_time'] = 'Waktu éksékusi: _EXECUTIONTIME_ detik';
+$messages['versions_found'] = 'Kapanggih _NUMBEROFVERSIONS_ vérsi';
+$messages['please_wait'] = 'Tunguan heula ...';
+$messages['binary_test'] = 'Ngabandingkeun bédana dina _FIRSTDATEVERSION_ antara _FIRSTNUMBER_ jeung _SECONDNUMBER_ nu asalna tina _SOURCENUMBER_:';
+$messages['dead_end'] = 'Saeutik kapahung (sigana alatan pamalikan atawa perang éditan)';
+$messages['once_more'] = 'Sakali deui, maké parasaan:';
+$messages['here'] = 'didieu';
+$messages['help_translating'] = 'Tulungan narjamahkeun na translatewiki.net';
+$messages['start_here'] = 'Paluruh ti dieu';
