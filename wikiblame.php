225d71707b395a39f3c0831424dd5eef51d868ef
Waldir Pimenta <waldyrious@gmail.com>
2017-06-27 18:02:09 +0100

    fix typo: Änderungen --> Änderung

--- wikiblame.php
+++ wikiblame.php
@@ -601,7 +601,7 @@ function listversions ($history)
 			<a href="/wiki/Spezial:Beitr%C3%A4ge/TXiKiBoT" title="Spezial:Beiträge/TXiKiBoT">Beiträge</a>)
 		</span>
 	</span> 
-	<abbr class="minor" title="Kleine Änderungen">K</abbr> 
+	<abbr class="minor" title="Kleine Änderung">K</abbr> 
 	<span class="history-size">(10.740 Bytes)</span> 
 	<span class="comment">(Bot: Ergänze: <a href="http://vi.wikipedia.org/wiki/Hinterzarten" class="extiw" title="vi:Hinterzarten">vi:Hinterzarten</a>)</span> (<span class="mw-history-undo"><a href="/w/index.php?title=Hinterzarten&amp;action=edit&amp;undoafter=64556690&amp;undo=64569839" title="Hinterzarten">entfernen</a></span>) </span> <small><span class='fr-hist-autoreviewed plainlinks'>[<a href="http://de.wikipedia.org/w/index.php?title=Hinterzarten&amp;stableid=64569839" class="external text" rel="nofollow">automatisch gesichtet</a>]</span></small></li> <li><span class='flaggedrevs-color-1'>(<a href="/w/index.php?title=Hinterzarten&amp;diff=64569839&amp;oldid=64556690" title="Hinterzarten">Aktuell</a>) (<a href="/w/index.php?title=Hinterzarten&amp;diff=64556690&amp;oldid=63484457" title="Hinterzarten">Vorherige</a>) <input type="radio" value="64556690" checked="checked" name="oldid" id="mw-oldid-64556690" /><input type="radio" value="64556690" 
 	</li>	*/
