f646c36bfb28350b294d496454fe3ff2d9097364
translatewiki.net <l10n-bot@translatewiki.net>
2017-08-14 08:35:55 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/ug-arab.php
+++ wikiblame_inc/ug-arab.php
@@ -11,6 +11,7 @@
  * @file
  *
  * @author Sahran
+ * @author Uzdil
  */
 
 $messages['January'] = 'يانۋار';
@@ -27,12 +28,12 @@ $messages['November'] = 'نويابىر';
 $messages['December'] = 'دېكابىر';
 $messages['ui_lang'] = 'كۆرسىتىدىغان تىللار';
 $messages['lang'] = 'تىل';
-$messages['project'] = 'قۇرۇلۇش';
+$messages['project'] = 'تۈرلەر';
 $messages['article'] = 'بەت';
-$messages['needle'] = 'ئىزدە';
-$messages['skipversions'] = 'ھەمىشە x نەشرىدىن ئاتلا';
-$messages['ignorefirst'] = 'تۇنجى x نەشرىگە پەرۋا قىلما';
-$messages['limit'] = 'تەكشۈرىدىغان نەشرلىرى';
+$messages['needle'] = 'ئىزدەش';
+$messages['skipversions'] = 'ھەمىشە x نەشرىدىن ئاتلىسۇن';
+$messages['ignorefirst'] = 'تۇنجى x نەشرىگە پەرۋا قىلمىسۇن';
+$messages['limit'] = 'تەكشۈرىدىغان نەشىرلىرى';
 $messages['start_date'] = 'باشلانغان چېسلا';
 $messages['date_format'] = 'MM -DD  - يىلىYYYY';
 $messages['order'] = 'تەرتىپى';
@@ -44,8 +45,13 @@ $messages['binary'] = 'ئىككىلىك';
 $messages['linear'] = 'سىزىقلىق';
 $messages['interpolated'] = 'ئىككىلىك (تېخىمۇ كۆپ نەشرى ئىچىدە تېخىمۇ تېز)';
 $messages['ignore_minors'] = 'ئازراقلا تۈزىتىشكە پەرۋا قىلما (تەجرىبە خاراكتىرلىق ئىقتىدار)';
-$messages['force_wikitags'] = 'مەجبۇرىي wikitext ئىزدە';
-$messages['start'] = 'باشلا';
-$messages['reset'] = 'ئەسلىگە قايتۇر';
+$messages['force_wikitags'] = 'مەجبۇرىي wikitext ئىزدەش';
+$messages['start'] = 'باشلاش';
+$messages['reset'] = 'ئەسلىگە قايتۇرۇش';
 $messages['manual'] = 'قوللانما';
 $messages['contact'] = 'ئالاقە';
+$messages['please_wait'] = 'سەل تەخىر قىلىڭ ...';
+$messages['once_more'] = 'يەنە بىر قېتىم:';
+$messages['here'] = 'بۇ يەردە';
+$messages['help_translating'] = 'Translatewiki.net تىكى ياردەم تەرجىمىسى';
+$messages['start_here'] = 'بۇ يەردىن باشلاپ ئىزدەش';
