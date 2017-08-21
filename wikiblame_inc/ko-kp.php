379591b521669e9fdc36c14bcb259736e75fbc05
translatewiki.net <l10n-bot@translatewiki.net>
2017-08-21 07:52:19 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/ko-kp.php
@@ -0,0 +1,40 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** 한국어 (조선) (한국어 (조선))
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Delim
+ */
+
+$messages['January'] = '1월';
+$messages['February'] = '2월';
+$messages['March'] = '3월';
+$messages['April'] = '4월';
+$messages['May'] = '5월';
+$messages['June'] = '6월';
+$messages['July'] = '7월';
+$messages['August'] = '8월';
+$messages['September'] = '9월';
+$messages['October'] = '10월';
+$messages['November'] = '11월';
+$messages['December'] = '12월';
+$messages['ui_lang'] = '표시 언어';
+$messages['lang'] = '언어';
+$messages['project'] = '과제';
+$messages['article'] = '문서';
+$messages['needle'] = '검색어';
+$messages['skipversions'] = '항상 몇 판을 건너뛰기';
+$messages['ignorefirst'] = '처음 몇 판을 무시';
+$messages['limit'] = '확인할 판의 개수';
+$messages['start_date'] = '시작할 날짜';
+$messages['date_format'] = 'YYYY년 MM월 DD일';
+$messages['order'] = '순서';
+$messages['newest_first'] = '최근 항목 먼저';
+$messages['oldest_first'] = '과거 항목 먼저';
