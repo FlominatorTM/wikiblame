a13b47394b9d762eb51612c40605227772e6db5b
translatewiki.net <l10n-bot@translatewiki.net>
2017-08-24 09:44:13 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/kab.php
+++ wikiblame_inc/kab.php
@@ -10,6 +10,7 @@
  * @ingroup Language
  * @file
  *
+ * @author Belkacem77
  * @author Mezgoug
  * @author Mmistmurt
  */
@@ -26,8 +27,28 @@ $messages['September'] = 'Ctamber';
 $messages['October'] = 'Tuber';
 $messages['November'] = 'Wamber';
 $messages['December'] = 'Dujamber';
+$messages['ui_lang'] = 'Sken tutlayt';
 $messages['lang'] = 'Tameslayt';
 $messages['project'] = 'Asenfar';
 $messages['article'] = 'Asebter';
 $messages['needle'] = 'Nadi';
+$messages['skipversions'] = 'Yal tikelt ngez x ileqman';
+$messages['ignorefirst'] = 'Zgel x n ileqman imenza';
+$messages['limit'] = 'Ileqman ara tesneqdeḍ';
 $messages['start_date'] = 'Azemz n tazwara';
+$messages['order'] = 'Asmizzwer';
+$messages['newest_first'] = 'melmi kan d amezwaru';
+$messages['oldest_first'] = 'aqbuṛ d amezwaru';
+$messages['search_method'] = 'Tarrayt n unadi';
+$messages['binary'] = 'imsin';
+$messages['start'] = 'Bdu';
+$messages['reset'] = 'Ales awennez';
+$messages['manual'] = 'Awfus';
+$messages['contact'] = 'Anermes';
+$messages['earlier_versions_available'] = 'Ahat llan ileqman iwburen.';
+$messages['please_wait'] = 'Rǧu...';
+$messages['insertion_found'] = 'Taguri gar LEFT_VERSION akked RIGHT_VERSION';
+$messages['deletion_found'] = 'Tukksa gar LEFT_VERSION akked RIGHT_VERSION';
+$messages['here'] = 'dagi';
+$messages['help_translating'] = 'Mud afus i tsuqilt di translatewiki.net';
+$messages['start_here'] = 'Nadi s yagi';
