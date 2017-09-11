8e989d18eeb792138796c3df3687e6140be0a868
translatewiki.net <l10n-bot@translatewiki.net>
2017-09-11 07:31:54 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/mwl.php
@@ -0,0 +1,27 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Mirandese (Mirandés)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author MokaAkashiyaPT
+ */
+
+$messages['January'] = 'janeiro';
+$messages['February'] = 'febreiro';
+$messages['March'] = 'márcio';
+$messages['April'] = 'abril';
+$messages['May'] = 'maio';
+$messages['June'] = 'júnio';
+$messages['July'] = 'júlio';
+$messages['August'] = 'agosto';
+$messages['September'] = 'setembre';
+$messages['October'] = 'outubre';
+$messages['November'] = 'nobembre';
+$messages['December'] = 'dezembre';
