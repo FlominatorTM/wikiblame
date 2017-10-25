8ebc304c2e43ac000dac17ebe1e8a4bdcdddb681
FlominatorTM <flominator@gmx.net>
2017-10-25 19:53:05 +0200

    extracting method prevent_automatic_escaping_of_input_strings()

--- shared_inc/wiki_functions.inc.php
+++ shared_inc/wiki_functions.inc.php
@@ -530,4 +530,27 @@ function print_debug($str)
     }
 }
 
+
+function prevent_automatic_escaping_of_input_strings()
+{
+    // required to prevent automatic escaping of input strings
+    // thanks to http://www.php.net/manual/de/security.magicquotes.disabling.php	
+    if (get_magic_quotes_gpc()) 
+    {
+        function stripslashes_deep($value)
+        {
+            $value = is_array($value) ?
+                        array_map('stripslashes_deep', $value) :
+                        stripslashes($value);
+
+            return $value;
+        }
+
+        $_POST = array_map('stripslashes_deep', $_POST);
+        $_GET = array_map('stripslashes_deep', $_GET);
+        $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+        $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
+    }
+}
+
 ?>
--- wikiblame.php
+++ wikiblame.php
@@ -9,27 +9,9 @@ while (@ob_end_flush());/* no output buffering (via http://stackoverflow.com/a/1
 		<link rel="shortcut icon" href="WikiBlame.png" type="image/png">
 		<title>WikiBlame</title>
 	</head><?php 
-
-// required to prevent automatic escaping of input strings
-// thanks to http://www.php.net/manual/de/security.magicquotes.disabling.php	
-if (get_magic_quotes_gpc()) {
-    function stripslashes_deep($value)
-    {
-        $value = is_array($value) ?
-                    array_map('stripslashes_deep', $value) :
-                    stripslashes($value);
-
-        return $value;
-    }
-
-    $_POST = array_map('stripslashes_deep', $_POST);
-    $_GET = array_map('stripslashes_deep', $_GET);
-    $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
-    $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
-}
-	
 include("shared_inc/language.inc.php");
 include("shared_inc/wiki_functions.inc.php");
+prevent_automatic_escaping_of_input_strings();
 $inc_dir = "wikiblame_inc";
 
 
