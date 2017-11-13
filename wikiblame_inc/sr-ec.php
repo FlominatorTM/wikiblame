542aedcf1e783eb9935e578bff629dd7815cd027
translatewiki.net <l10n-bot@translatewiki.net>
2017-11-13 09:29:57 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/sr-ec.php
+++ wikiblame_inc/sr-ec.php
@@ -15,6 +15,7 @@
  * @author Milicevic01
  * @author Obsuser
  * @author Od1n
+ * @author Prevodim
  * @author Rancher
  * @author Михајло Анђелковић
  */
@@ -56,6 +57,9 @@ $messages['linear'] = 'линеаран';
 $messages['interpolated'] = 'бинаран (брже с више издања)';
 $messages['ignore_minors'] = 'Занемари мање измене (пробно)';
 $messages['force_wikitags'] = 'Приморај тражење викитекста';
+$messages['from_url'] = 'из URL-а';
+$messages['paste_url'] = 'Залепите URL ка Медијавики страници';
+$messages['no_valid_url'] = 'Нема валидног Медијавики URL-а';
 $messages['start'] = 'Започни';
 $messages['reset'] = 'Поништи';
 $messages['manual'] = 'Приручник';
@@ -68,6 +72,8 @@ $messages['search_in_progress_text'] = 'Историја издања чланк
 $messages['search_in_progress_wikitags'] = 'Историја издања чланка _ARTICLELINK_ се тражи за <b>_NEEDLE_</b> као викитекст';
 $messages['no_differences'] = 'Нису пронађене разлике у прегледаним изменама.';
 $messages['inverse_restart'] = 'Није пронађено убацивање или уклањање; је ли термин претраге убачен касније?';
+$messages['inverse_stuck'] = 'Нису пронађена додавања или уклањања у ових _NUMBEROFVERSIONS_ измена. Да ли је појам за претрагу можда уклоњен раније?';
+$messages['inverse_earliest'] = 'Претрага у ранијим изменама';
 $messages['first_version'] = 'Промена се десила у првој или последњој измени?';
 $messages['first_version_present'] = '__NEEDLE__ већ постоји у најстаријој претраживаној измени, од датума __REVISIONLINK__.';
 $messages['earlier_versions_available'] = 'Вероватно постоје и старија издања.';
@@ -77,7 +83,8 @@ $messages['please_wait'] = 'Сачекајте…';
 $messages['binary_test'] = 'Поређење разлика у _FIRSTDATEVERSION_ између _FIRSTNUMBER_ и _SECONDNUMBER_ што долази из _SOURCENUMBER_:';
 $messages['dead_end'] = 'Претрага је дошла до мртве тачке. Узрок је вероватно враћање измена или уређивачки рат.';
 $messages['once_more'] = 'Поновљена претрага:';
-$messages['delete_from_here'] = 'Брисање _NUMBEROFVERSIONS_ ранијих измена, пошто је уклањање извршено касније';
+$messages['delete_from_here'] = 'Брисање _NUMBEROFVERSIONS_ ранијих измена, пошто уклањање мора да буде извршено касније';
+$messages['delete_until_here'] = 'Брисање _NUMBEROFVERSIONS_ каснијих измена, пошто уклањање мора да буде извршено раније';
 $messages['binary_enough'] = 'Извршено је довољно покушаја, историја чланка је збркана. Промените неке поставке.';
 $messages['insertion_found'] = 'Унос је пронађен између LEFT_VERSION и RIGHT_VERSION';
 $messages['deletion_found'] = 'Брисање је пронађено између LEFT_VERSION и RIGHT_VERSION';
