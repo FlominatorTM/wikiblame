31146b898c31c008374cf4d6d24133f2adf35b39
translatewiki.net <l10n-bot@translatewiki.net>
2018-03-19 21:10:41 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/hyw.php
@@ -0,0 +1,36 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** արեւմտահայերէն (արեւմտահայերէն)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Armeniki
+ */
+
+$messages['January'] = 'Յունուար';
+$messages['February'] = 'Փետրուար';
+$messages['March'] = 'Մարտ';
+$messages['April'] = 'Ապրիլ';
+$messages['May'] = 'Մայիս';
+$messages['June'] = 'Յունիս';
+$messages['July'] = 'Յուլիս';
+$messages['August'] = 'Օգոստոս';
+$messages['September'] = 'Սեպտեմբեր';
+$messages['October'] = 'Հոկտեմբեր';
+$messages['November'] = 'Նոյեմբեր';
+$messages['December'] = 'Դեկտեմբեր';
+$messages['ui_lang'] = 'Ցուցադրութեան լեզու';
+$messages['lang'] = 'Լեզու';
+$messages['project'] = 'Առաջարկ';
+$messages['article'] = 'Էջ';
+$messages['needle'] = 'Որոնել';
+$messages['start_date'] = 'Սկզբնաւորութեան Թուական';
+$messages['date_format'] = 'DD/MM/YYYY';
+$messages['start'] = 'Սկսիլ';
+$messages['here'] = 'այստեղ';
