07a1e90274eb7b14bede371f0205f4d6864ffeb6
translatewiki.net <l10n-bot@translatewiki.net>
2018-05-03 09:38:04 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/ady-cyrl.php
+++ wikiblame_inc/ady-cyrl.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** адыгабзэ (адыгабзэ)
+/** Adyghe (Cyrillic script) (адыгабзэ)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
