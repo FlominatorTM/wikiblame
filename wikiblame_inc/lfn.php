8f8503dc887e8bbe4f749680cf0c794a2c04fc9c
translatewiki.net <l10n-bot@translatewiki.net>
2018-05-14 08:28:01 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/lfn.php
@@ -0,0 +1,17 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Lingua Franca Nova (Lingua Franca Nova)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Robin van der Vliet
+ */
+
+$messages['lang'] = 'Lingua';
+$messages['project'] = 'Projeta';
