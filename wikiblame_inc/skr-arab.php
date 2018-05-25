b90ae1d8a6143389b6c879bf7206852e472a3bb1
translatewiki.net <l10n-bot@translatewiki.net>
2018-05-25 12:00:40 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/skr-arab.php
+++ wikiblame_inc/skr-arab.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** سرائیکی (سرائیکی)
+/** Saraiki (Arabic script) (سرائیکی)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
