0190bbad75bf9074e7b508c40894031bbc5fa14a
translatewiki.net <l10n-bot@translatewiki.net>
2018-11-15 08:27:58 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/be-tarask.php
+++ wikiblame_inc/be-tarask.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** be-tarask (be-tarask)
+/** Belarusian (Taraškievica orthography) (беларуская (тарашкевіца)‎)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
--- wikiblame_inc/yue.php
+++ wikiblame_inc/yue.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** yue (yue)
+/** Cantonese (粵語)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
