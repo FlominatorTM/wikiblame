37e9463437846dacf7f7b0ba090eab7445e1c8c1
translatewiki.net <l10n-bot@translatewiki.net>
2019-01-21 10:09:59 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/ksw.php
@@ -0,0 +1,26 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** စှီၤ ကညီကျိာ် (စှီၤ ကညီကျိာ်)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Rul1902
+ */
+
+$messages['January'] = 'ယနူၤအါရံၤဝါရီ';
+$messages['February'] = 'ဖ့ၣ်ဘြူၤအါရံၤ';
+$messages['March'] = 'မာ်ၡး';
+$messages['April'] = 'အ့ဖြ့ၣ်';
+$messages['May'] = 'မ့ၤ';
+$messages['June'] = 'ယူၤ';
+$messages['July'] = 'ယူၤလံ';
+$messages['August'] = 'အီကူး';
+$messages['September'] = 'အီးကထိဘၢၣ်';
+$messages['November'] = 'နိၣ်ဝ့ဘၢၣ်';
+$messages['December'] = 'စံၣ်စဲဘၢၣ်';
