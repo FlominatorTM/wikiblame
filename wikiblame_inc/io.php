732dccb8e9ecd5a8032fa0af55e4171f98141ad5
translatewiki.net <l10n-bot@translatewiki.net>
2019-03-07 16:26:36 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/io.php
@@ -0,0 +1,16 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Ido (Ido)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Joao Xavier
+ */
+
+$messages['search_in_progress_text'] = '<b>_NEEDLE_</b> serchesis kom simpla texto (sen formato) en la modifikuri di _ARTICLELINK_';
