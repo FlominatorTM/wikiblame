7d157012da2062549edd50ea6fb3e648fb8029a0
translatewiki.net <l10n-bot@translatewiki.net>
2019-03-18 09:00:01 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/gcr.php
+++ wikiblame_inc/gcr.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** kréyòl gwiyanè (kréyòl gwiyanè)
+/** Guianan Creole (kriyòl gwiyannen)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
