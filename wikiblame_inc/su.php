81e078de694f5c454f040bc9c7cfe6f76eabdd8f
translatewiki.net <l10n-bot@translatewiki.net>
2019-09-06 07:15:34 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/su.php
+++ wikiblame_inc/su.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** Sundanese (Basa Sunda)
+/** Sundanese (Sunda)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
