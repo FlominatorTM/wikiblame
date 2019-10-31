1dcd8cd1de0e26df70730e2c484c9efb4f48284a
translatewiki.net <l10n-bot@translatewiki.net>
2019-10-31 18:51:59 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/fit.php
@@ -0,0 +1,55 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Tornedalen Finnish (meänkieli)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Pyscowicz
+ */
+
+$messages['January'] = 'Janyaari';
+$messages['February'] = 'Fepryaari';
+$messages['March'] = 'Marsi';
+$messages['April'] = 'Aprilli';
+$messages['May'] = 'Mai';
+$messages['June'] = 'Jyyni';
+$messages['July'] = 'Jyyli';
+$messages['August'] = 'Aykysti';
+$messages['September'] = 'Septemperi';
+$messages['October'] = 'Oktuuperi';
+$messages['November'] = 'Nuvemperi';
+$messages['December'] = 'Tesemperi';
+$messages['ui_lang'] = 'Näyttökieli';
+$messages['lang'] = 'Kieli';
+$messages['project'] = 'Prujekti';
+$messages['article'] = 'Sivu';
+$messages['needle'] = 'Hae tekstiä';
+$messages['skipversions'] = 'Ohita aina x versuunia';
+$messages['ignorefirst'] = 'Ohita ensimmäiset x versuunia';
+$messages['limit'] = 'Läpikäytävien versuunien lukumäärä';
+$messages['start_date'] = 'Alkamistaatumi';
+$messages['date_format'] = 'DD.MM.YYYY';
+$messages['revision_date_format'] = '%d. %B %Y, %H:%M';
+$messages['order'] = 'Lajittele';
+$messages['newest_first'] = 'uusimat ensin';
+$messages['oldest_first'] = 'vanhimat ensin';
+$messages['search_method'] = 'Hakumetuuti';
+$messages['binary'] = 'binaarinen';
+$messages['linear'] = 'lineaarinen';
+$messages['interpolated'] = 'binaari (nopeampi, jos versuuneja on paljon)';
+$messages['force_wikitags'] = 'Pakota haku wikitekstistä';
+$messages['from_url'] = 'URL-atressista';
+$messages['paste_url'] = 'Liitä url-atressi MeetiaWiki-sivule';
+$messages['no_valid_url'] = 'Tämä ei ole kelvollinen MeetiaWiki url-atressi';
+$messages['start'] = 'Alota';
+$messages['reset'] = 'Tännään';
+$messages['manual'] = 'Ohje';
+$messages['contact'] = 'Ota yhteyttä';
+$messages['please_wait'] = 'Oota...';
+$messages['here'] = 'täälä';
