4e81e97c3095bcb22bac055a99171da6a229fac3
translatewiki.net <l10n-bot@translatewiki.net>
2020-02-10 15:01:00 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/ti.php
@@ -0,0 +1,29 @@
+<?php
+/** WikiBlame
+ *
+ */
+/** Tigrinya (ትግርኛ)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Joanmp17
+ */
+
+$messages['January'] = 'ጥሪ';
+$messages['February'] = 'ለካቲት';
+$messages['March'] = 'መጋቢት';
+$messages['April'] = 'ሚያዝያ';
+$messages['May'] = 'ግንቦት';
+$messages['June'] = 'ሰነ';
+$messages['July'] = 'ሓምለ';
+$messages['August'] = 'ነሓሰ';
+$messages['September'] = 'መስከረም';
+$messages['October'] = 'ጥቅምቲ';
+$messages['November'] = 'ሕዳር';
+$messages['December'] = 'ታሕሳስ';
+$messages['lang'] = 'ቋንቋ';
+$messages['article'] = 'ገጽ';
