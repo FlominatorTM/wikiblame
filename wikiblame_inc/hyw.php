4940b55816d9de536ab59a3835f25811b3d9e0e4
translatewiki.net <l10n-bot@translatewiki.net>
2020-04-20 17:04:00 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/hyw.php
+++ wikiblame_inc/hyw.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** Western Armenian (արեւմտահայերէն)
+/** Western Armenian (Արեւմտահայերէն)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
--- wikiblame_inc/zgh.php
+++ wikiblame_inc/zgh.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** Standard Moroccan Tamazight (ⵜⴰⵎⴰⵣⵉⵖⵜ)
+/** Standard Moroccan Tamazight (ⵜⴰⵎⴰⵣⵉⵖⵜ ⵜⴰⵏⴰⵡⴰⵢⵜ)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
