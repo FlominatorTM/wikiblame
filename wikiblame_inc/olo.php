4c3f80381f1ce8731f136c538351478c94585452
translatewiki.net <l10n-bot@translatewiki.net>
2020-12-17 15:24:12 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/olo.php
+++ wikiblame_inc/olo.php
@@ -2,7 +2,7 @@
 /** WikiBlame
  *
  */
-/** Livvi-Karelian (Livvinkarjala)
+/** Livvi-Karelian (livvinkarjala)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
