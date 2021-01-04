c06e369ff73a0a295d14a8840554c93a5db8ef4b
translatewiki.net <l10n-bot@translatewiki.net>
2021-01-04 14:55:34 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/th.php
+++ wikiblame_inc/th.php
@@ -10,6 +10,7 @@
  * @ingroup Language
  * @file
  *
+ * @author Aefgh39622
  * @author Octahedron80
  * @author กิ๊ฟ เลิกล่ะ สายแข็ง
  */
@@ -50,15 +51,28 @@ $messages['start'] = 'เริ่ม';
 $messages['reset'] = 'ตั้งค่าใหม่';
 $messages['manual'] = 'ตั้งค่าเอง';
 $messages['contact'] = 'ติดต่อ';
-$messages['search_in_progress_text'] = 'ประวัติรุ่นของ _ARTICLELINK_ กำลังค้นหา <b>_NEEDLE_</b> ด้วยข้อความเรียบ';
+$messages['get_less_versions'] = 'การค้นหาของคุณอาจสอบถามรุ่นแก้ไข __NUMREVISIONS__ รุ่นในครั้งเดียว เพื่อเป็นการปกป้องเซิร์ฟเวอร์ คุณสามารถสอบถามได้เพียง __ALLOWEDREVISIONS__ รุ่นต่อหนึ่งครั้งที่เรียกเท่านั้น โปรดเปลี่ยนการตั้งค่าหรือสลับวิธีการค้นหาเป็นแบบไบนารี!';
+$messages['wrong_skips'] = 'การตั้งค่าไม่ถูกต้อง: หาก __VERSIONSTOSKIP__ รุ่นแรกถูกข้ามไป ระบบจะไม่ประมวลผล __VERSIONSTOSEARCH__ รุ่นที่จะค้นหา';
+$messages['search_in_progress_text'] = 'ประวัติรุ่นของ _ARTICLELINK_ กำลังค้นหา <b>_NEEDLE_</b> ด้วยข้อความธรรมดา';
 $messages['search_in_progress_wikitags'] = 'ประวัติรุ่นของ _ARTICLELINK_ กำลังค้นหา <b>_NEEDLE_</b> ด้วยข้อความวิกิ';
 $messages['no_differences'] = 'ไม่พบความแตกต่างในรุ่นที่ค้นหา';
 $messages['first_version'] = 'การเปลี่ยนแปลงต้องปรากฏในรุ่นแรกหรือรุ่นล่าสุดหรือไม่';
-$messages['earlier_versions_available'] = 'เป็นไปได้ว่ามีรุ่นก่อนหน้านี้';
+$messages['first_version_present'] = '__NEEDLE__ มีอยู่แล้วในรุ่นแก้ไขที่เก่าที่สุดจากเมื่อ __REVISIONLINK__ ที่พบ';
+$messages['latest_version_present'] = '__NEEDLE__ มีอยู่แล้วในรุ่นแก้ไขที่ใหม่ที่สุดจากเมื่อ __REVISIONLINK__ ที่พบ';
+$messages['earlier_versions_available'] = 'เป็นไปได้ว่ามีรุ่นที่เก่ากว่านี้';
 $messages['execution_time'] = 'เวลาดำเนินการ: _EXECUTIONTIME_ วินาที';
-$messages['versions_found'] = '_NUMBEROFVERSIONS_ รุ่นที่พบ';
+$messages['versions_found'] = 'พบ _NUMBEROFVERSIONS_ รุ่น';
 $messages['please_wait'] = 'กรุณารอสักครู่ …';
-$messages['insertion_found'] = 'การเพิ่มที่พบระหว่าง LEFT_VERSION และ RIGHT_VERSION';
+$messages['binary_test'] = 'กำลังเปรียบเทียบความแตกต่างใน _FIRSTDATEVERSION_ ระหว่าง _FIRSTNUMBER_ และ _SECONDNUMBER_ ขณะมาจาก _SOURCENUMBER_:';
+$messages['dead_end'] = 'ตรวจพบจุดสิ้นสุดที่ไม่ถูกต้องบางส่วน (อาจเป็นเนื่องจากการย้อนหรือสงครามการแก้ไข)';
+$messages['once_more'] = 'อีกครั้งด้วยความรู้สึก:';
+$messages['delete_from_here'] = 'กำลังลบรุ่นแก้ไข _NUMBEROFVERSIONS_ รุ่นที่เก่ากว่า เนื่องจากการเอาออกจะต้องทำในภายหลัง';
+$messages['delete_until_here'] = 'กำลังลบรุ่นแก้ไข _NUMBEROFVERSIONS_ รุ่นที่ใหม่กว่า เนื่องจากการแทรกจะต้องทำเร็วกว่านี้';
+$messages['binary_enough'] = 'พยายามหลายครั้งจนมากพอแล้ว ประวัติบทความค่อนข้างจะกระจัดกระจาย โปรดลองเปลี่ยนการตั้งค่าบางอย่าง';
+$messages['insertion_found'] = 'การแทรกที่พบระหว่าง LEFT_VERSION และ RIGHT_VERSION';
 $messages['deletion_found'] = 'การลบที่พบระหว่าง LEFT_VERSION และ RIGHT_VERSION';
+$messages['here'] = 'ที่นี่';
 $messages['help_translating'] = 'ช่วยแปลที่ translatewiki.net';
 $messages['start_here'] = 'ค้นหาจากที่นี่';
+$messages['too_much_versions'] = 'คุณได้สอบถามถึงขีดจำกัดที่ __VERSIONLIMIT__ รุ่นแล้ว โปรดลองอีกครั้งในอีก __WAITMINUTES__ นาทีหรือสลับไปยังการค้นหาแบบไบนารี ขออภัยในความไม่สะดวก';
+$messages['not_found_at_all'] = 'ไม่พบคำค้นหาของคุณ ตรวจสอบการตั้งค่าแล้วลองอีกครั้ง';
