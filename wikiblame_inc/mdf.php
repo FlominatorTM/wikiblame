5233e97042293ddaf905e1a0df35ec2342928a68
translatewiki.net <l10n-bot@translatewiki.net>
2021-05-27 15:54:16 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/mdf.php
@@ -0,0 +1,13 @@
+<?php
+/** Moksha (мокшень)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Kareyac
+ */
+
+$messages['February'] = 'Уфайков';
