fdf868f84e8da47fea6c1569000d1775f84b12e6
translatewiki.net <l10n-bot@translatewiki.net>
2021-09-23 13:13:45 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/blk.php
@@ -0,0 +1,14 @@
+<?php
+/** ပအိုဝ်ႏ (ပအိုဝ်ႏ)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author 咽頭べさ
+ */
+
+$messages['January'] = 'စန်နဝါရီ';
+$messages['April'] = 'အေပီ';
