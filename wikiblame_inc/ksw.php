4d4db8ef4e76d6a4781bbaa483c4d299cf10c340
translatewiki.net <l10n-bot@translatewiki.net>
2021-10-18 13:13:12 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/ksw.php
+++ wikiblame_inc/ksw.php
@@ -1,5 +1,5 @@
 <?php
-/** စှီၤ ကညီကျိာ် (စှီၤ ကညီကျိာ်)
+/** စှီၤ (စှီၤ)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit https://translatewiki.net
@@ -24,3 +24,19 @@ $messages['September'] = 'လါစဲးပတ့ဘၢၣ်';
 $messages['October'] = 'လါအီးကထိဘၢၣ်';
 $messages['November'] = 'လါနိၣ်ဝ့ဘၢၣ်';
 $messages['December'] = 'လါဒံၣ်စဲဘၢၣ်';
+$messages['ui_lang'] = 'ကျိာ်ပာ်ဖျါ';
+$messages['lang'] = 'ကျိာ်';
+$messages['project'] = 'တၢ်တိာ်ကျဲၤ';
+$messages['article'] = 'ကဘျံးပၤ';
+$messages['needle'] = 'ကွၢ်ဃုထၢ';
+$messages['skipversions'] = 'ထီဘိန့ၣ်ခီပတာ် x versions';
+$messages['ignorefirst'] = 'ဟးဆှဲးဆိ x versions';
+$messages['start_date'] = 'စးထီၣ်မုၢ်နံၤမုၢ်သီ';
+$messages['date_format'] = 'MM DD, YYYY';
+$messages['revision_date_format'] = '%H:%M, %d %B %Y';
+$messages['start'] = 'စးထီၣ်';
+$messages['reset'] = 'စးထီၣ်ခီၣ်ထံး';
+$messages['contact'] = 'တၢ်ဆဲးကျိး';
+$messages['please_wait'] = 'ဝံသးစူၤ အိၣ်ခိး...';
+$messages['here'] = 'လၢအံၤ';
+$messages['start_here'] = 'ကွၢ်ဃုလၢအံၤ';
