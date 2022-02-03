9d9a56e81165e0b7c11112896c4f630c4d6d3461
translatewiki.net <l10n-bot@translatewiki.net>
2022-02-03 13:14:12 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/ksw.php
+++ wikiblame_inc/ksw.php
@@ -1,5 +1,5 @@
 <?php
-/** စှီၤ (စှီၤ)
+/** S'gaw Karen (စှီၤ)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit https://translatewiki.net
