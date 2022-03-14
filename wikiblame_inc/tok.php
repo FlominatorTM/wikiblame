591d547cb7677fd406d1c4aeb94c7f90eace95f8
translatewiki.net <l10n-bot@translatewiki.net>
2022-03-14 13:14:59 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/tok.php
@@ -0,0 +1,13 @@
+<?php
+/** toki pona (toki pona)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Robin van der Vliet
+ */
+
+$messages['lang'] = 'toki';
