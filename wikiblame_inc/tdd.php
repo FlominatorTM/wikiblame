8c1a6b8f4e102ad752641727ffc6a8f8085e421c
translatewiki.net <l10n-bot@translatewiki.net>
2022-06-09 13:18:59 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/tdd.php
+++ wikiblame_inc/tdd.php
@@ -1,5 +1,5 @@
 <?php
-/** ᥖᥭᥰᥖᥬᥳᥑᥨᥒᥰ (ᥖᥭᥰᥖᥬᥳᥑᥨᥒᥰ)
+/** Tai Nuea (ᥖᥭᥰᥖᥬᥳᥑᥨᥒᥰ)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit https://translatewiki.net
