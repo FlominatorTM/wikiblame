f40bac339b5198f5c9ef6af014ea2c3ed0c0426e
translatewiki.net <l10n-bot@translatewiki.net>
2022-06-23 13:19:49 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/sro.php
@@ -0,0 +1,29 @@
+<?php
+/** sardu campidanesu (sardu campidanesu)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author F Samaritani
+ */
+
+$messages['January'] = 'Gennàrgiu';
+$messages['February'] = 'Friàrgiu';
+$messages['March'] = 'Martzu';
+$messages['April'] = 'Abrili';
+$messages['May'] = 'Maju';
+$messages['June'] = 'Làmpadas';
+$messages['July'] = 'Mesi de argiolas';
+$messages['August'] = 'Austu';
+$messages['September'] = 'Cabudanni';
+$messages['October'] = 'Santu Aini';
+$messages['November'] = 'Donniassantu';
+$messages['December'] = 'Mesi de idas';
+$messages['lang'] = 'Lìngua';
+$messages['project'] = 'Cadela';
+$messages['article'] = 'Pàgina';
+$messages['date_format'] = 'DD MM, YYYY';
+$messages['start'] = 'Inghitza';
