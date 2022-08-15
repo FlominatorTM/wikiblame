94d1bd48ef8fdcbebcbd0d836d7c289a7fa70b26
translatewiki.net <l10n-bot@translatewiki.net>
2022-08-15 13:14:38 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/yi.php
+++ wikiblame_inc/yi.php
@@ -7,9 +7,23 @@
  * @ingroup Language
  * @file
  *
+ * @author Pacha Tchernof
  * @author פוילישער
  */
 
+$messages['January'] = 'יאַנואַר';
+$messages['February'] = 'פֿעברואַר';
+$messages['March'] = 'מערץ';
+$messages['April'] = 'אַפּריל';
+$messages['May'] = 'מאַי';
+$messages['June'] = 'יוני';
+$messages['July'] = 'יולי';
+$messages['August'] = 'אויגוסט';
+$messages['September'] = 'סעפּטעמבער';
+$messages['October'] = 'אָקטאָבער';
+$messages['November'] = 'נאָוועמבער';
+$messages['December'] = 'דעצעמבער';
+$messages['article'] = 'בלאַט';
 $messages['start_date'] = 'אָנהייב דאַטע';
 $messages['reset'] = 'צוריקשטעלן';
 $messages['earlier_versions_available'] = 'עס זענען פֿאראן ווארשיינלעך עלטערע ווערסיעס.';
