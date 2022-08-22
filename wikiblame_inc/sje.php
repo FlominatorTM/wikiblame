9476699a86c30a283a40f1d744eb5e73fda403f7
translatewiki.net <l10n-bot@translatewiki.net>
2022-08-22 13:15:06 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/sje.php
@@ -0,0 +1,33 @@
+<?php
+/** Pite Sami (bidumsámegiella)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Olve Utne
+ */
+
+$messages['January'] = 'åđđåjakmánno';
+$messages['February'] = 'gåvvåmánno';
+$messages['March'] = 'njukktjamánno';
+$messages['April'] = 'vuoratjismánno';
+$messages['May'] = 'mårbmesmánno';
+$messages['June'] = 'biehtsemánno';
+$messages['July'] = 'sjnjilltjamánno';
+$messages['August'] = 'bårrgemánno';
+$messages['September'] = 'ragatmánno';
+$messages['October'] = 'gålgådismánno';
+$messages['November'] = 'bassemánno';
+$messages['December'] = 'jåvvlåmánno';
+$messages['ui_lang'] = 'Vuosedimgiella';
+$messages['lang'] = 'Giella';
+$messages['project'] = 'Prosjäkkta';
+$messages['tld'] = 'Domiena';
+$messages['tld_example'] = 'org, com, net, …';
+$messages['article'] = 'Bielle';
+$messages['needle'] = 'Åtså';
+$messages['start_date'] = 'Állgembäjjve';
+$messages['start_here'] = 'Åtså dásste';
