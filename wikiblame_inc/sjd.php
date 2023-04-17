a9df0e62f538754f117060958a4b0e9ef1aba163
translatewiki.net <l10n-bot@translatewiki.net>
2023-04-17 13:17:02 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/sjd.php
@@ -0,0 +1,13 @@
+<?php
+/** Kildin Sami (кӣллт са̄мь кӣлл)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Merrahtar
+ */
+
+$messages['revision_date_format'] = '%H:%M, %d. %B %Y';
