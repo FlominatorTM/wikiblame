718952a219cc44f28915746dd770624344f09fbd
translatewiki.net <l10n-bot@translatewiki.net>
2023-07-20 13:17:58 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/udm.php
+++ wikiblame_inc/udm.php
@@ -9,6 +9,7 @@
  *
  * @author AlnashPiyash2
  * @author Shklyaev
+ * @author Света Поторочина
  */
 
 $messages['January'] = 'Толшор';
@@ -23,9 +24,14 @@ $messages['September'] = 'куарусёнэ';
 $messages['October'] = 'коньывуонэ';
 $messages['November'] = 'шуркынмонэ';
 $messages['December'] = 'толсуре';
+$messages['ui_lang'] = 'Возьматон кыл';
 $messages['lang'] = 'Кыл';
 $messages['project'] = 'Проект';
+$messages['tld'] = 'Домен';
+$messages['tld_example'] = 'org, com
+, net, …';
 $messages['article'] = 'Бам';
+$messages['needle'] = 'утчан чур';
 $messages['start_date'] = 'Кутсконлэн датаез';
 $messages['newest_first'] = 'нырысь выльзэ';
 $messages['here'] = 'татын';
