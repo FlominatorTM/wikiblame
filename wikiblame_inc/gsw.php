ae65f09c4c017223f70f5eec327d913f4a04b532
translatewiki.net <l10n-bot@translatewiki.net>
2023-08-17 13:26:30 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/gsw.php
+++ wikiblame_inc/gsw.php
@@ -1,5 +1,5 @@
 <?php
-/** Swiss German (Alemannisch)
+/** Alemannic (Alemannisch)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit https://translatewiki.net
