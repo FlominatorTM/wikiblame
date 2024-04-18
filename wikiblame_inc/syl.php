37aca53998f8f40858f00e86fdbd3b216d24f641
translatewiki.net <l10n-bot@translatewiki.net>
2024-04-18 14:22:37 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/syl.php
@@ -0,0 +1,24 @@
+<?php
+/** Sylheti (ꠍꠤꠟꠐꠤ)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author ꠢꠣꠍꠘ ꠞꠣꠎꠣ
+ */
+
+$messages['January'] = 'ꠎꠣꠘꠥꠀꠞꠤ';
+$messages['February'] = 'ꠚꠦꠛꠥꠀꠞꠤ';
+$messages['March'] = 'ꠝꠣꠞ꠆ꠌ';
+$messages['April'] = 'ꠄꠙ꠆ꠞꠤꠟ';
+$messages['May'] = 'ꠝꠦ';
+$messages['June'] = 'ꠎꠥꠘ';
+$messages['July'] = 'ꠎꠥꠟꠣꠁ';
+$messages['August'] = 'ꠀꠉꠡ꠆ꠐ';
+$messages['September'] = 'ꠍꠦꠙ꠆ꠐꠦꠝ꠆ꠛꠞ';
+$messages['October'] = 'ꠅꠇ꠆ꠐꠥꠛꠞ';
+$messages['November'] = 'ꠘꠜꠦꠝ꠆ꠛꠞ';
+$messages['December'] = 'ꠒꠤꠡꠦꠝ꠆ꠛꠞ';
