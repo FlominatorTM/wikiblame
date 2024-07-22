18a6393360585543211e85a4de664718ab6e6060
translatewiki.net <l10n-bot@translatewiki.net>
2024-07-22 14:20:53 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/isv-latn.php
@@ -0,0 +1,83 @@
+<?php
+/** medžuslovjansky (medžuslovjansky)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author IJzeren Jan
+ */
+
+$messages['January'] = 'januar';
+$messages['February'] = 'fevruar';
+$messages['March'] = 'marec';
+$messages['April'] = 'aprilj';
+$messages['May'] = 'maj';
+$messages['June'] = 'junij';
+$messages['July'] = 'julij';
+$messages['August'] = 'avgust';
+$messages['September'] = 'septembr';
+$messages['October'] = 'oktobr';
+$messages['November'] = 'novembr';
+$messages['December'] = 'dekembr';
+$messages['ui_lang'] = 'Jezyk izobraženja';
+$messages['lang'] = 'Jezyk';
+$messages['project'] = 'Projekt';
+$messages['tld'] = 'Domen';
+$messages['tld_example'] = 'org, com, net, …';
+$messages['article'] = 'Stranica';
+$messages['needle'] = 'Poiskati';
+$messages['skipversions'] = 'Vsegda prěskočiti x versij';
+$messages['ignorefirst'] = 'Ignorovati prvyh x versij';
+$messages['limit'] = 'Versije za prověrku';
+$messages['start_date'] = 'Data početka';
+$messages['date_format'] = 'DD.MM.YYYY';
+$messages['revision_date_format'] = '%H:%M, %d %B %Y';
+$messages['order'] = 'Poredok';
+$messages['newest_first'] = 'prvo najnovějše';
+$messages['oldest_first'] = 'prvo najstarějše';
+$messages['binary_search_inverse'] = 'Iskati odstranjenje teksta (samo dvojično)';
+$messages['search_method'] = 'Metoda iskanja';
+$messages['binary'] = 'dvojična';
+$messages['linear'] = 'linearna';
+$messages['interpolated'] = 'dvojična (brzěje, ako jest mnogo versij)';
+$messages['ignore_minors'] = 'Ignorovati male pravky (eksperimentalno)';
+$messages['force_wikitags'] = 'Nasilno iskati vikitekst';
+$messages['from_url'] = 's URL-adresa';
+$messages['paste_url'] = 'Prosimo, vstavite URL-adres MediaWiki-stranice';
+$messages['no_valid_url'] = 'Tuto ne jest pravilny URL-adres MediaWiki';
+$messages['start'] = 'Začeti';
+$messages['reset'] = 'Resetovati';
+$messages['manual'] = 'Priručnik';
+$messages['contact'] = 'Kontakt';
+$messages['source_code'] = 'Izvorny kod na GitHub';
+$messages['get_less_versions'] = 'Vaš zapyt može jednočasno proiskati maksimalno __NUMREVISIONS__. V cěljah ohrany servera dozvaljaje se vam proiskati samo __ALLOWEDREVISIONS__ versij na jedin zapyt. Prosimo, izměnite nastavjenja ili prěključite metodu iskanja na dvojičnu!';
+$messages['wrong_skips'] = 'Nepravilne nastavjenja: ako prve __VERSIONSTOSKIP__ versije sut prěskočene, nijedna versija iz __VERSIONSTOSEARCH__ ne bude obrabotana.';
+$messages['search_in_progress_text'] = 'Iskaje se <b>_NEEDLE_</b> kako obyčny tekst v historiji versij stranice _ARTICLELINK_';
+$messages['search_in_progress_wikitags'] = 'Iskaje se <b>_NEEDLE_</b> kako vikitekst v historiji versij stranice _ARTICLELINK_';
+$messages['no_differences'] = 'Nijedna razlika ne najdena v proiskanyh versijah.';
+$messages['inverse_restart'] = 'Nijedna vstavka ili odstranjenje ne najdeno. Može li byti, že poiskany tekst byl vstavjeny pozdněje?';
+$messages['inverse_stuck'] = 'Nijedna vstavka ili odstranjenje ne najdeno v tutyh _NUMBEROFVERSIONS_ versijah. Može li byti, že poiskany tekst byl odstranjeny raněje?';
+$messages['inverse_earliest'] = 'Iskati v ranějših versijah';
+$messages['first_version'] = 'Izměna iměla odbyti se v prvoj ili poslědjej versiji?';
+$messages['first_version_present'] = '__NEEDLE__ uže nahodilo se v najstarějšej srěd proiskanyh versij od __REVISIONLINK__.';
+$messages['latest_version_present'] = '__NEEDLE__ uže nahodilo se v najnovějšej srěd proiskanyh versij od __REVISIONLINK__.';
+$messages['earlier_versions_available'] = 'Pravdopodobno sut starějše versije.';
+$messages['execution_time'] = 'Čas izpolnjenja: _EXECUTIONTIME_ sekund';
+$messages['versions_found'] = 'Najdeno _NUMBEROFVERSIONS_ versij';
+$messages['please_wait'] = 'Prosimo, počekajte...';
+$messages['binary_test'] = 'Sravnjenje razlik v versiji _FIRSTDATEVERSION_ medžu versijami _FIRSTNUMBER_ i _SECONDNUMBER_, proizhodeči s _SOURCENUMBER_:';
+$messages['dead_end'] = 'Mrtva točka (pravdopodobno iz pričiny odčinjenij ili vojen pravok)';
+$messages['once_more'] = 'Ješče raz, s čutjem:';
+$messages['delete_from_here'] = 'Odstranjanje _NUMBEROFVERSIONS_ ranějših versij, ibo udaljenje imělo byti sdělano pozdněje';
+$messages['delete_until_here'] = 'Odstranjanje _NUMBEROFVERSIONS_ pozdnějših versij, ibo vstavjenje imělo byti sdělano pozdněje';
+$messages['binary_enough'] = 'Izpolnjeno mnogokratnyh pokušenij, ale historija članka jest strašno haotična. Prosimo, poprobujte izměniti někoje parametry.';
+$messages['insertion_found'] = 'Najdeno vstavjenje medžu LEFT_VERSION i RIGHT_VERSION';
+$messages['deletion_found'] = 'Najdeno odstranjenje medžu LEFT_VERSION i RIGHT_VERSION';
+$messages['here'] = 'tu';
+$messages['help_translating'] = 'Pomožite s prěvodženjem na translatewiki.net';
+$messages['start_here'] = 'Iskati odsud';
+$messages['too_much_versions'] = 'Vy dosegli jeste do limita zapytov po kolikosti __VERSIONLIMIT__ versij. Prosimo, poprobujte ponovno poslě __WAITMINUTES__ minut ili prěključite se v dvojično iskanje. Izvinite za neudobstvo.';
+$messages['not_found_at_all'] = 'Poiskany vami izraz sovsim ne byl najden. Prověrite nastavjenja i poprobujte ponovno.';
