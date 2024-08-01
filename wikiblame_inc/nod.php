e99989a23d8f5162bc330c6c48eda1ac51baa899
translatewiki.net <l10n-bot@translatewiki.net>
2024-08-01 14:18:07 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/nod.php
@@ -0,0 +1,13 @@
+<?php
+/** Northern Thai (ᨣᩤᩴᨾᩮᩬᩥᨦ)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Noktonissian
+ */
+
+$messages['manual'] = 'ᨠᩣᩴᩉ᩠ᨶᩫ᩠ᨯ​ᨤᩫ᩠ᨶ​ᨯ᩠ᨿᩅ';
