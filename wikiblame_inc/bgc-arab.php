4aa91d4831ae10f39a49f1a4bafed07c760f1c1f
translatewiki.net <l10n-bot@translatewiki.net>
2024-10-03 14:18:26 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/bgc-arab.php
@@ -0,0 +1,13 @@
+<?php
+/** Haryanvi (Arabic script) (ہریانوی)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author امین اکبر
+ */
+
+$messages['article'] = 'پنا';
--- wikiblame_inc/map-bms.php
+++ wikiblame_inc/map-bms.php
@@ -1,5 +1,5 @@
 <?php
-/** Basa Banyumasan (Basa Banyumasan)
+/** Banyumasan (Basa Banyumasan)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit https://translatewiki.net
