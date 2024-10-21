25e139cb574917732bdadf0f305fccb3a927f534
translatewiki.net <l10n-bot@translatewiki.net>
2024-10-21 14:19:17 +0200

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/ace.php
+++ wikiblame_inc/ace.php
@@ -1,5 +1,5 @@
 <?php
-/** Achinese (Acèh)
+/** Acehnese (Acèh)
  * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit https://translatewiki.net
