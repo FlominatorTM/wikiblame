46a9bc7d260b02e93d0495c1838ac1285c3f21f7
translatewiki.net <l10n-bot@translatewiki.net>
2024-10-24 14:21:11 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/tig.php
@@ -0,0 +1,25 @@
+<?php
+/** Tigre (ትግሬ)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Bbeshir
+ */
+
+$messages['January'] = 'የናየር';
+$messages['February'] = 'ፈብራየር';
+$messages['March'] = 'ማርች';
+$messages['April'] = 'አብሪል';
+$messages['May'] = 'ማዮ';
+$messages['June'] = 'ጁን';
+$messages['July'] = 'ጁላይ';
+$messages['August'] = 'አቅስጦስ';
+$messages['September'] = 'ሴፕቴምበር';
+$messages['October'] = 'ኦክቶበር';
+$messages['November'] = 'ኖቨምበር';
+$messages['December'] = 'ዲሰምበር';
+$messages['here'] = 'እንዠ';
