4d643c80e0cae51a48fd3a8b595cb68a4b2867b1
translatewiki.net <l10n-bot@translatewiki.net>
2025-07-31 14:27:55 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/prg.php
@@ -0,0 +1,34 @@
+<?php
+/** Prussian (prūsiskan)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Kxeo
+ */
+
+$messages['January'] = 'Rags';
+$messages['February'] = 'Wassarins';
+$messages['March'] = 'Pūlis';
+$messages['April'] = 'Sakkis';
+$messages['May'] = 'Zallaws';
+$messages['June'] = 'Sīmenis';
+$messages['July'] = 'Līpa';
+$messages['August'] = 'Daggis';
+$messages['September'] = 'Sillins';
+$messages['October'] = 'Spallins';
+$messages['November'] = 'Lapkrūtis';
+$messages['December'] = 'Sallaws';
+$messages['ui_lang'] = 'Etwaidīnsnas bilā';
+$messages['lang'] = 'Bilā';
+$messages['project'] = 'Prōjaktan';
+$messages['tld'] = 'Dōmeni';
+$messages['tld_example'] = 'org, com, net, …';
+$messages['article'] = 'Pāusan';
+$messages['needle'] = 'Laukīs';
+$messages['skipversions'] = 'Wissadan izlaišais x wersiōnis';
+$messages['ignorefirst'] = 'Ignōris pirmāi x wersiōnis';
+$messages['limit'] = 'Wersiōnis izbandātun';
