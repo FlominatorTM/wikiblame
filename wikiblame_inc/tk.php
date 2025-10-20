1acaf40aa2d738774534f77f05d3d37e4602eb84
translatewiki.net <l10n-bot@translatewiki.net>
2025-10-20 07:40:47 +0200

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/tk.php
@@ -0,0 +1,24 @@
+<?php
+/** Turkmen (Türkmençe)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Kxeo
+ */
+
+$messages['January'] = 'Ýanwar';
+$messages['February'] = 'Fewral';
+$messages['March'] = 'Mart';
+$messages['April'] = 'Aprel';
+$messages['May'] = 'Maý';
+$messages['June'] = 'Iýun';
+$messages['July'] = 'Iýul';
+$messages['August'] = 'Awgust';
+$messages['September'] = 'Sentýabr';
+$messages['October'] = 'Oktýabr';
+$messages['November'] = 'Noýabr';
+$messages['December'] = 'Dekabr';
