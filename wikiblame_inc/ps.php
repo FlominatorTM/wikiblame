6057349fab2c5f56656ba14e3842128186ddca71
translatewiki.net <l10n-bot@translatewiki.net>
2026-01-22 13:27:38 +0100

    Localisation updates from https://translatewiki.net.

--- wikiblame_inc/ps.php
+++ wikiblame_inc/ps.php
@@ -8,23 +8,28 @@
  * @file
  *
  * @author Ahmed-Najib-Biabani-Ibrahimkhel
+ * @author شاه زمان پټان
  */
 
+$text_dir = 'rtl';
 $messages['January'] = 'جنوري';
 $messages['February'] = 'فبروري';
 $messages['March'] = 'مارچ';
-$messages['April'] = 'اپرېل';
+$messages['April'] = 'اپريل';
 $messages['May'] = 'می';
 $messages['June'] = 'جون';
 $messages['July'] = 'جولای';
-$messages['August'] = 'اگسټ';
+$messages['August'] = 'اگست';
 $messages['September'] = 'سېپتمبر';
 $messages['October'] = 'اکتوبر';
 $messages['November'] = 'نومبر';
-$messages['December'] = 'ډيسمبر';
+$messages['December'] = 'ډېسمبر';
 $messages['ui_lang'] = 'د ښکارېدنې ژبه';
 $messages['lang'] = 'ژبه';
+$messages['lang_example'] = 'ps, commons, www, …';
 $messages['project'] = 'پروژه';
+$messages['tld'] = 'واک‌سيمه';
+$messages['tld_example'] = 'org, com, net, …';
 $messages['article'] = 'مخ';
 $messages['needle'] = 'پلټنه د';
 $messages['skipversions'] = 'تل له x بڼو تېرېدل';
@@ -41,9 +46,11 @@ $messages['linear'] = 'ليکه ايز';
 $messages['interpolated'] = 'دوه ايز (په ډېرو بڼو سره چټک دی)';
 $messages['ignore_minors'] = 'واړه بدلونونه بابېزل (آزمېښتي)';
 $messages['start'] = 'پيلول';
-$messages['reset'] = 'بياايښودل';
+$messages['reset'] = 'له‌سره‌اوڼل';
 $messages['manual'] = 'لارښوونيز';
 $messages['contact'] = 'اړيکه';
-$messages['please_wait'] = 'لطفاً تم شۍ ...';
+$messages['please_wait'] = 'مهرباني وکړئ په تمه شئ...';
+$messages['here'] = 'دلته';
 $messages['help_translating'] = 'په translatewiki.net کې په ژباړې کې مرسته وکړۍ';
 $messages['start_here'] = 'له دې ځايه پلټل';
+$messages['not_found_at_all'] = 'ستاسو لټون گړنې ونه موندل شوې. اوڼنې وگورئ او بيا هڅه وکړئ.';
