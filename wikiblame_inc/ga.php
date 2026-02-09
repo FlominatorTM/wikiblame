9a79df647a0aff886ae37c0837c7947c0894a45c
translatewiki.net <l10n-bot@translatewiki.net>
2026-02-09 13:22:39 +0100

    Localisation updates from https://translatewiki.net.

---
+++ wikiblame_inc/ga.php
@@ -0,0 +1,83 @@
+<?php
+/** Irish (Gaeilge)
+ * 
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Aindriu80
+ */
+
+$messages['January'] = 'Eanáir';
+$messages['February'] = 'Feabhra';
+$messages['March'] = 'Márta';
+$messages['April'] = 'Aibreán';
+$messages['May'] = 'Bealtaine';
+$messages['June'] = 'Meitheamh';
+$messages['July'] = 'Iúil';
+$messages['August'] = 'Lúnasa';
+$messages['September'] = 'Meán Fómhair';
+$messages['October'] = 'Deireadh Fómhair';
+$messages['November'] = 'Samhain';
+$messages['December'] = 'Nollaig';
+$messages['ui_lang'] = 'Teanga taispeána';
+$messages['lang'] = 'Teanga';
+$messages['project'] = 'Tionscadal';
+$messages['tld'] = 'Fearann';
+$messages['tld_example'] = 'org, com, net, …';
+$messages['article'] = 'Leathanach';
+$messages['needle'] = 'Cuardaigh';
+$messages['skipversions'] = 'Seachain leaganacha x i gcónaí';
+$messages['ignorefirst'] = 'Déan neamhaird de na chéad leaganacha x';
+$messages['limit'] = 'Leaganacha le seiceáil';
+$messages['start_date'] = 'Dáta tosaithe';
+$messages['date_format'] = 'DD MM, YYYY';
+$messages['revision_date_format'] = '%H:%M, %d %B %Y';
+$messages['order'] = 'Ordú';
+$messages['newest_first'] = 'is déanaí ar dtús';
+$messages['oldest_first'] = 'is sine ar dtús';
+$messages['binary_search_inverse'] = 'Lorg baint téacs (dénártha amháin)';
+$messages['search_method'] = 'Modh cuardaigh';
+$messages['binary'] = 'dénártha';
+$messages['linear'] = 'líneach';
+$messages['interpolated'] = 'dénártha (níos tapúla le níos mó leaganacha)';
+$messages['ignore_minors'] = 'Déan neamhaird ar athruithe beaga (turgnamhach)';
+$messages['force_wikitags'] = 'Cuardach fórsaithe le haghaidh téacs vicí';
+$messages['from_url'] = 'ó URL';
+$messages['paste_url'] = 'Greamaigh an URL chuig leathanach MediaWiki le do thoil';
+$messages['no_valid_url'] = 'Ní URL MediaWiki bailí ar bith é seo';
+$messages['start'] = 'Tosaigh';
+$messages['reset'] = 'Athshocraigh';
+$messages['manual'] = 'Lámhleabhar';
+$messages['contact'] = 'Teagmháil';
+$messages['source_code'] = 'Cód foinse ar GitHub';
+$messages['get_less_versions'] = 'D’fhéadfadh do chuardach __NUMREVISIONS__ athbhreithniú a iarraidh ag an am céanna. Chun an freastalaí a chosaint, ní cheadaítear duit ach __ALLOWEDREVISIONS__ a iarraidh in aghaidh an ghlao. Athraigh na socruithe nó aistrigh an modh cuardaigh go dénártha le do thoil!';
+$messages['wrong_skips'] = 'Socruithe míchearta: Má scipeáiltear na chéad leaganacha __VERSIONSTOSKIP__, ní phróiseálfar aon cheann de na leaganacha __VERSIONSTOSEARCH__ atá le cuardach.';
+$messages['search_in_progress_text'] = 'Tá stair leaganacha _ARTICLELINK_ á chuardach le haghaidh <b>_NEEDLE_</b> mar théacs simplí';
+$messages['search_in_progress_wikitags'] = 'Tá stair leaganacha _ARTICLELINK_ á chuardach le haghaidh <b>_NEEDLE_</b> mar théacs vicí';
+$messages['no_differences'] = 'Ní bhfuarthas aon difríochtaí sna hathbhreithnithe a cuardaíodh.';
+$messages['inverse_restart'] = 'Níor aimsíodh aon chur isteach ná baint, an ndearnadh an téarma cuardaigh a chur isteach níos déanaí?';
+$messages['inverse_stuck'] = 'Níor aimsíodh aon chur isteach ná baint sna _NUMBEROFVERSIONS_ athbhreithnithe seo. Ar baineadh an téarma cuardaigh níos luaithe b\'fhéidir?';
+$messages['inverse_earliest'] = 'Cuardaigh i leasuithe níos luaithe';
+$messages['first_version'] = 'An gcaithfidh gur tharla an t-athrú sa chéad athbhreithniú nó sa athbhreithniú is déanaí?';
+$messages['first_version_present'] = 'Bhí __NEEDLE__ i láthair cheana féin sa leagan is sine a cuardaíodh, dar dáta __REVISIONLINK__.';
+$messages['latest_version_present'] = 'Bhí __NEEDLE__ i láthair cheana féin sa leagan is óige a cuardaíodh, dar dáta __REVISIONLINK__.';
+$messages['earlier_versions_available'] = 'Is dócha go bhfuil sean-athbhreithnithe ann.';
+$messages['execution_time'] = 'Am forghníomhaithe: _EXECUTIONTIME_ soicind';
+$messages['versions_found'] = '_NUMBEROFVERSIONS_ leagan aimsithe';
+$messages['please_wait'] = 'Fan le do thoil…';
+$messages['binary_test'] = 'Ag comparáid idir difríochtaí i _FIRSTDATEVERSION_ idir _FIRSTNUMBER_ agus _SECONDNUMBER_ agus iad ag teacht ó _SOURCENUMBER_:';
+$messages['dead_end'] = 'Gabhadh pointe éigin gan réiteach (is dócha gur tharla sé de bharr aisiompuithe nó cogaí eagarthóireachta)';
+$messages['once_more'] = 'Arís eile, le mothúchán:';
+$messages['delete_from_here'] = 'Ag scriosadh _NUMBEROFVERSIONS_ athbhreithnithe níos luaithe, ós rud é gur gá iad a bhaint níos déanaí';
+$messages['delete_until_here'] = 'Ag scriosadh _NUMBEROFVERSIONS_ athbhreithnithe níos déanaí, ós rud é gur cheart go mbeadh an t-ionchur déanta níos luaithe';
+$messages['binary_enough'] = 'Rinneadh go leor iarrachtaí eile, tá stair an ailt sách mearbhall, déan iarracht roinnt socruithe a athrú le do thoil.';
+$messages['insertion_found'] = 'Ionsá aimsithe idir LEFT_VERSION agus RIGHT_VERSION';
+$messages['deletion_found'] = 'Scriosadh aimsithe idir LEFT_VERSION agus RIGHT_VERSION';
+$messages['here'] = 'anseo';
+$messages['help_translating'] = 'Cabhair le haistriúchán ag translatewiki.net';
+$messages['start_here'] = 'Cuardaigh as seo';
+$messages['too_much_versions'] = 'Tá teorainn do chuardaigh de __VERSIONLIMIT__ leaganacha sroichte agat. Déan iarracht arís i gceann __WAITMINUTES__ nóiméad nó aistrigh go cuardach dénártha. Gabh mo leithscéal as an míchaoithiúlacht.';
+$messages['not_found_at_all'] = 'Ní bhfuarthas do théarma cuardaigh ar chor ar bith. Seiceáil na socruithe agus déan iarracht arís.';
